44b532b2296b8cc467635787c5f8f6a14f95b4ed
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-21 15:53:56 +0500

    updated main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import './assets/main.css'
+// Импоритируем вручную, иначе выкидывает ошибку при использовании ElConfigProvider
+import { ElSelect, ElOption } from 'element-plus';
+import 'element-plus/es/components/select/style/css';
+import 'element-plus/es/components/option/style/css';
+import './assets/main.css';
 
-const app = createApp(App)
+import apiPlugin from '@/plugins/api.plugin.js';
 
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .use(apiPlugin)
+  .component('ElSelect', ElSelect)
+  .component('ElOption', ElOption)
+  .mount('#app');
