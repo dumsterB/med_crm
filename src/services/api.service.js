bc402cb38681ad71b9a9051c2cc832852dd93d4a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-21 15:29:12 +0500

    api.service.js created

---
+++ src/services/api.service.js
@@ -0,0 +1,45 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  mode: 'no-cors',
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+});
+
+instance.setToken = token => {
+  instance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+instance.removeToken = () => {
+  delete instance.defaults.headers.common.Authorization;
+};
+
+instance.interceptors.request.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  res => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return res;
+  },
+  err => {
+    if (err?.response?.status === 401) {
+      // store.dispatch('auth/logout');
+    }
+    if (err?.response?.status === 403) {
+      // store.dispatch("auth/logout");
+    }
+
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
