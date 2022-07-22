9645d64f77160d08bb0949af052861acbc9ad57f
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-22 10:13:13 +0500

    create vite.util.js

---
+++ src/utils/vite.util.js
@@ -0,0 +1,18 @@
+/**
+ * @param { string } url значение import.meta.url
+ * @return { string } возвращает название родительской папки
+ */
+export function getParentFolderNameByMetaUrl(url) {
+  const paths = new URL(url).pathname.split('/');
+  return paths[paths.length - 2];
+}
+
+/**
+ * Для vue компонентов
+ * @param { string } url значение import.meta.url
+ * @return {string} возвращает название файла
+ */
+export function getFileNameByMetaUrl(url) {
+  const paths = new URL(url).pathname.split('/');
+  return paths[paths.length - 1].split('.')[0];
+}
