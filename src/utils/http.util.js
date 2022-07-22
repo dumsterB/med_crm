f25f84fcb5a5a5ee62508e13f726e15e7b26beec
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-22 17:44:57 +0500

    create http util

---
+++ src/utils/http.util.js
@@ -0,0 +1,18 @@
+/**
+ * @private
+ * @param { string } url
+ * @param { object } query
+ * @return { string } return url with query
+ */
+export function mergeOrCreateQuery({ url, query }) {
+  const [urlExcludeQuery, inlineQueries] = url.split('?');
+
+  const queries = new URLSearchParams(
+    `${
+      (inlineQueries || '') +
+      Object.keys(query).reduce((acc, key) => acc + `&${key}=${query[key]}`, '')
+    }`
+  );
+
+  return `${urlExcludeQuery}?${queries.toString()}`;
+}
