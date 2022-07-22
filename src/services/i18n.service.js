fefe6d356e84c90db73fba2bc51f3da923ae85c3
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-22 21:09:39 +0500

    create i18n.service.js

---
+++ src/services/i18n.service.js
@@ -0,0 +1,37 @@
+import { I18nPlugin } from '@/plugins/i18n.plugin';
+import { DEFAULT_LOCALE, LOCALES } from '@/config/i18n.config';
+
+export class I18nService {
+  static locales = Object.keys(LOCALES).map(key => LOCALES[key]);
+  static LOCALE_STORAGE_KEY = 'LOCALE';
+
+  static t(payload) {
+    return I18nPlugin.global.t(payload);
+  }
+  static tc(payload) {
+    return I18nPlugin.global.tc(payload);
+  }
+  static te(payload) {
+    return I18nPlugin.global.te(payload);
+  }
+  static tm(payload) {
+    return I18nPlugin.global.tm(payload);
+  }
+
+  static getLocale() {
+    return I18nPlugin.global.locale;
+  }
+  static setLocale(locale) {
+    if (!this.checkHasLocale(locale)) {
+      console.error(`${locale} - incorrect locale.\nSupported locales - ${this.locales.join(',')}`);
+      locale = DEFAULT_LOCALE;
+    }
+    I18nPlugin.global.locale = locale;
+  }
+  static setLocaleFromStorage() {
+    this.setLocale(localStorage.getItem(this.LOCALE_STORAGE_KEY) || DEFAULT_LOCALE);
+  }
+  static checkHasLocale(locale) {
+    return this.locales.includes(locale);
+  }
+}
