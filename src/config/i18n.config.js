bd7ed6b590ec1bb0b49de91f0bca88dfc9521b8a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-22 19:04:53 +0500

    add comment in i18n.config.js

--- src/config/i18n.config.js
+++ src/config/i18n.config.js
@@ -1,6 +1,8 @@
+// Не менять, в json переводах используем ru, uz
 export const LOCALES = {
   RU: 'ru',
   UZ: 'uz',
 };
+
 export const DEFAULT_LOCALE = LOCALES.RU;
 export const FALLBACK_LOCALE = LOCALES.RU;
