e6c72a214e03defc762d0e82783a7bedc83e9996
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 15:28:31 +0500

    create empty VDashboard page

---
+++ src/views/registry/VDashboard/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <LayoutRegistry>
+    <router-link to="/auth/login">Login</router-link>
+  </LayoutRegistry>
+</template>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="./index.locales.json" />
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+import LayoutRegistry from '@/components/layouts/LayoutRegistry/index.vue';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  components: { LayoutRegistry },
+};
+</script>
