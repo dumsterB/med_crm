c6116ea6256ab333106a921c63e37362cf8a974a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 15:29:07 +0500

    create empty VForgotPasswordPage

---
+++ src/views/auth/VForgotPassword/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <LayoutAuth> Forgot page </LayoutAuth>
+</template>
+<style lang="scss" src="./index.scss" />
+
+<i18n src="./index.locales.json" />
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+import LayoutAuth from '@/components/layouts/LayoutAuth/index.vue';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  components: { LayoutAuth },
+};
+</script>
