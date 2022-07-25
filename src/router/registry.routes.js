47750082e0df022115c985c622ae6caa6478961d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 20:40:30 +0500

    use onlyLoggedInMiddleware for registry.routes.js

--- src/router/registry.routes.js
+++ src/router/registry.routes.js
@@ -1,9 +1,11 @@
 import VDashboard from '@/views/registry/VDashboard/index.vue';
+import { onlyLoggedInMiddleware } from '@/middlewares/onlyLoggedIn.middleware';
 
 export const REGISTRY_DASHBOARD_ROUTE = {
   name: 'REGISTRY_DASHBOARD',
   path: '/registry/dashboard',
   component: VDashboard,
+  beforeEnter: [onlyLoggedInMiddleware],
 };
 
 export const routes = [REGISTRY_DASHBOARD_ROUTE];
