0c055cf999146afa1701c38bc48d7bde6a2a212d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 09:29:55 +0500

    use I18nService.setLocaleFromStorage when App mounted

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,3 @@
-<script>
-import { getFileNameByMetaUrl } from '@/utils/vite.util';
-import { RouterView } from 'vue-router';
-import { ElConfigProvider } from 'element-plus';
-
-export default {
-  name: getFileNameByMetaUrl(import.meta.url),
-  components: { RouterView, ElConfigProvider },
-};
-</script>
-
 <template>
   <div class="app">
     <ElConfigProvider>
@@ -16,5 +5,21 @@ export default {
     </ElConfigProvider>
   </div>
 </template>
+
+<script>
+import { getFileNameByMetaUrl } from '@/utils/vite.util';
+import { RouterView } from 'vue-router';
+import { ElConfigProvider } from 'element-plus';
+import { I18nService } from '@/services/i18n.service';
+
+export default {
+  name: getFileNameByMetaUrl(import.meta.url),
+  components: { RouterView, ElConfigProvider },
+
+  mounted() {
+    I18nService.setLocaleFromStorage();
+  },
+};
+</script>
 
 <style src="./App.scss" lang="scss"></style>
