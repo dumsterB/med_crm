15377a26f00b5526a611115fb51e6cf1c5827797
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 20:33:32 +0500

    realized onlyLoggedOut.middleware.js

--- src/middlewares/onlyLoggedOut.middleware.js
+++ src/middlewares/onlyLoggedOut.middleware.js
@@ -1,3 +1,7 @@
+import { Store } from '@/store/index.js';
+import { REGISTRY_DASHBOARD_ROUTE } from '@/router/registry.routes';
+
 export const onlyLoggedOutMiddleware = function (to, from, next) {
+  if (Store.state.auth.isAuthorized) return next(REGISTRY_DASHBOARD_ROUTE.path);
   next();
 };
