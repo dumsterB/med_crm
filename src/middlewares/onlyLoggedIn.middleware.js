c9ded57872ddfb1c484a8869c9e5059a57879f82
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 20:40:10 +0500

    realized onlyLoggedIn.middleware.js

--- src/middlewares/onlyLoggedIn.middleware.js
+++ src/middlewares/onlyLoggedIn.middleware.js
@@ -1,3 +1,7 @@
+import { Store } from '@/store';
+import { LOGIN_ROUTE } from '@/router/auth.routes';
+
 export const onlyLoggedInMiddleware = function (to, from, next) {
-  next();
+  if (Store.state.auth.isAuthorized) return next();
+  next(LOGIN_ROUTE.path);
 };
