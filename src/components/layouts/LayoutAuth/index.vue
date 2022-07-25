163c34380f1afcf32814f57393a258a39ea1bf19
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 08:50:26 +0500

    create LayoutAuth

---
+++ src/components/layouts/LayoutAuth/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout-auth">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="scss" src="./index.scss" />
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+};
+</script>
