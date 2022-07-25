b5bb5352518fd01d4f447965b8ca7e8f4d14bd86
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 20:36:41 +0500

    use onlyLoggedOutMiddleware for auth routes

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -2,20 +2,25 @@ import VLogin from '@/views/auth/VLogin/index.vue';
 import VSignUp from '@/views/auth/VSignUp/index.vue';
 import VForgotPassword from '@/views/auth/VForgotPassword/index.vue';
 
+import { onlyLoggedOutMiddleware } from '@/middlewares/onlyLoggedOut.middleware';
+
 export const LOGIN_ROUTE = {
   name: 'AUTH_LOGIN',
   path: '/auth/login',
   component: VLogin,
+  beforeEnter: [onlyLoggedOutMiddleware],
 };
 export const SIGNUP_ROUTE = {
   name: 'AUTH_SIGNUP',
   path: '/auth/sign-up',
   component: VSignUp,
+  beforeEnter: [onlyLoggedOutMiddleware],
 };
 export const FORGOT_PASSWORD_ROUTE = {
   name: 'FORGOT_PASSWORD',
   path: '/auth/forgot-password',
   component: VForgotPassword,
+  beforeEnter: [onlyLoggedOutMiddleware],
 };
 
 export const routes = [LOGIN_ROUTE, SIGNUP_ROUTE, FORGOT_PASSWORD_ROUTE];
