f12c44d11bd5bb7c322fc6e2efbde1e07c294bfe
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-25 15:29:27 +0500

    add fogot page in auth routes

--- src/router/auth.routes.js
+++ src/router/auth.routes.js
@@ -1,24 +1,21 @@
-import V_Login from '@/views/auth/VLogin/index.vue';
-import V_SignUp from '@/views/auth/VSignUp/index.vue';
+import VLogin from '@/views/auth/VLogin/index.vue';
+import VSignUp from '@/views/auth/VSignUp/index.vue';
+import VForgotPassword from '@/views/auth/VForgotPassword/index.vue';
 
 export const LOGIN_ROUTE = {
   name: 'AUTH_LOGIN',
   path: '/auth/login',
+  component: VLogin,
 };
 export const SIGNUP_ROUTE = {
   name: 'AUTH_SIGNUP',
   path: '/auth/sign-up',
+  component: VSignUp,
+};
+export const FORGOT_PASSWORD_ROUTE = {
+  name: 'FORGOT_PASSWORD',
+  path: '/auth/forgot-password',
+  component: VForgotPassword,
 };
 
-export const routes = [
-  {
-    name: LOGIN_ROUTE.name,
-    path: LOGIN_ROUTE.path,
-    component: V_Login,
-  },
-  {
-    name: SIGNUP_ROUTE.name,
-    path: SIGNUP_ROUTE.path,
-    component: V_SignUp,
-  },
-];
+export const routes = [LOGIN_ROUTE, SIGNUP_ROUTE, FORGOT_PASSWORD_ROUTE];
