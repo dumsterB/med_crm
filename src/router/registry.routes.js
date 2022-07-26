885a901dabb2d16aba4c6ca784bc75962e633399
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-26 20:08:17 +0500

    add meta.title on registry routes

--- src/router/registry.routes.js
+++ src/router/registry.routes.js
@@ -9,24 +9,36 @@ export const REGISTRY_DASHBOARD_ROUTE = {
   path: '/registry/dashboard',
   component: VDashboard,
   beforeEnter: [onlyLoggedInMiddleware],
+  meta: {
+    title: 'Dashboard',
+  },
 };
 export const REGISTRY_PATIENTS_ROUTE = {
   name: 'REGISTRY_PATIENTS',
   path: '/registry/patients',
   component: VPatients,
   beforeEnter: [onlyLoggedInMiddleware],
+  meta: {
+    title: 'Patients',
+  },
 };
 export const REGISTRY_PATIENTS_RECORDS_ROUTE = {
   name: 'REGISTRY_PATIENTS_RECORDS',
   path: '/registry/patients-records',
   component: VPatientsRecords,
   beforeEnter: [onlyLoggedInMiddleware],
+  meta: {
+    title: 'Patients Records',
+  },
 };
 export const REGISTRY_DOCTORS_SCHEDULE_ROUTE = {
   name: 'REGISTRY_DOCTORS_SCHEDULE',
   path: '/registry/doctors-schedule',
   component: VDoctorsSchedule,
   beforeEnter: [onlyLoggedInMiddleware],
+  meta: {
+    title: 'Doctors Schedule',
+  },
 };
 
 export const routes = [
