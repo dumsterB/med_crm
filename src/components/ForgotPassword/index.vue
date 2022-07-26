dacf8c9b7d36ac871766e9c211988d752d35603d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-26 13:28:36 +0500

    create ForgotPassword component

---
+++ src/components/ForgotPassword/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="forgot-password">
+    <div class="forgot-password__title">{{ $t('Title') }}</div>
+    <div class="forgot-password__content">{{ $t('Content') }}</div>
+    <ElButton type="primary" @click="$emit('action:prev')">
+      {{ $t('Prev') }}
+    </ElButton>
+  </div>
+</template>
+<style lang="scss" src="./index.scss" />
+
+<i18n src="./index.locales.json" />
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  emits: ['action:prev'],
+};
+</script>
