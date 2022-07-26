a2342b846f0a749972a165269055f99a12830126
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-26 13:28:57 +0500

    realized VForgotPassword page

--- src/views/auth/VForgotPassword/index.vue
+++ src/views/auth/VForgotPassword/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <LayoutAuth> Forgot page </LayoutAuth>
+  <LayoutAuth content-class="v-forgot-password">
+    <img class="v-forgot-password__image" src="@/assets/images/forgot-password.png" alt="image" />
+    <ForgotPassword class="v-forgot-password__content" @action:prev="prevHandler" />
+  </LayoutAuth>
 </template>
 <style lang="scss" src="./index.scss" />
 
@@ -8,9 +11,16 @@
 <script>
 import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
 import LayoutAuth from '@/components/layouts/LayoutAuth/index.vue';
+import ForgotPassword from '@/components/ForgotPassword/index.vue';
 
 export default {
   name: getParentFolderNameByMetaUrl(import.meta.url),
-  components: { LayoutAuth },
+  components: { LayoutAuth, ForgotPassword },
+
+  methods: {
+    prevHandler() {
+      this.$router.go(-1);
+    },
+  },
 };
 </script>
