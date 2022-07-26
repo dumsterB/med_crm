db1c3be8f82b4a0c72879a93eed95a66e6766518
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-26 17:31:01 +0500

    create RegistryMenu component

---
+++ src/components/registry/RegistryMenu/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="registry-menu">
+    <img class="registry-menu__logo" src="@/assets/images/logo_medium.png" alt="logo" />
+
+    <nav class="registry-menu__nav registry-menu-nav">Nav</nav>
+
+    <div class="registry-menu__actions registry-menu-actions">
+      <ElButton link @click="logout"> {{ $t('Base.Logout') }} </ElButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  methods: {
+    logout() {
+      this.$store.dispatch('auth/logout');
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/base.locales.json" />
+<i18n src="./index.locales.json" />
