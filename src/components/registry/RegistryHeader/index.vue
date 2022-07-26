3a691dfd1d3e355914e024fd23e9be3ea947a8d8
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-26 17:31:17 +0500

    create RegistryHeader component

---
+++ src/components/registry/RegistryHeader/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="registry-header">Header</div>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="./index.locales.json" />
