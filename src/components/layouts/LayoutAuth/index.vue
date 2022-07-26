bad5041325206ed2d2a5d3b45c61d199e6728f07
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-26 12:10:43 +0500

    update LayoutAuth component

--- src/components/layouts/LayoutAuth/index.vue
+++ src/components/layouts/LayoutAuth/index.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="layout-auth">
-    <slot></slot>
+    <ElHeader :class="['layout-auth__header', 'layout-auth-header', headerClass]">
+      <div class="layout-auth-header__actions">
+        <LangSelect />
+      </div>
+    </ElHeader>
+
+    <div :class="['layout-auth__content', contentClass]">
+      <slot></slot>
+    </div>
   </div>
 </template>
-
 <style lang="scss" src="./index.scss" />
 
 <script>
 import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+import LangSelect from '@/components/LangSelect/index.vue';
 
 export default {
   name: getParentFolderNameByMetaUrl(import.meta.url),
+  components: { LangSelect },
+  props: {
+    contentClass: String,
+    headerClass: String,
+  },
 };
 </script>
