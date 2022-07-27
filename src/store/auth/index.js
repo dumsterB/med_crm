d26801f4e02305473262b0bbdcda862448e15875
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-27 14:10:15 +0500

    fix auth/logout action

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -56,6 +56,7 @@ export default {
     logout({ commit }) {
       commit('LOGOUT');
       ApiService.removeToken();
+      localStorage.removeItem('access_token');
       Router.push(LOGIN_ROUTE);
     },
   },
