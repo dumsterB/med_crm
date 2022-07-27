c2b1f811663c368591feef0866bea84c97ab438e
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-27 15:55:03 +0500

    Patient extends User

--- src/models/Patient.model.js
+++ src/models/Patient.model.js
@@ -1,22 +1,15 @@
-import { CRUDModel } from '@/models/CRUD.model';
+import { User } from '@/models/User.model';
 
 /**
  * @class Patient
  * @extends CRUDModel
  */
-export class Patient extends CRUDModel {
+export class Patient extends User {
   static modelName = 'patient';
   static tableName = 'patients';
   constructor(payload) {
     super(payload);
-    this.name = payload?.name || null;
-    this.email = payload?.email || null;
-    this.phone = payload?.phone || null;
-    this.birhdate = payload?.birhdate || null;
-    this.gender = payload?.gender || null;
-    this.role = payload?.role || null;
     this.childrens_count = payload?.childrens_count || 0;
     this.parent_id = payload?.parent_id || null;
-    this.created_at = payload?.created_at || null;
   }
 }
