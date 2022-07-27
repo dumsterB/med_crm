bdb7ee3a4441a51b6fd5058611f24e08887c287b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-27 12:56:25 +0500

    use loadGlobalLocales in i18n.plugin.js

--- src/plugins/i18n.plugin.js
+++ src/plugins/i18n.plugin.js
@@ -1,7 +1,9 @@
 import { createI18n } from 'vue-i18n';
 import { DEFAULT_LOCALE, FALLBACK_LOCALE } from '@/config/i18n.config';
+import { loadGlobalLocales } from '@/utils/i18n.utils';
 
 export const I18nPlugin = createI18n({
   locale: DEFAULT_LOCALE,
   fallbackLocale: FALLBACK_LOCALE,
+  messages: loadGlobalLocales(),
 });
