44924f3dd402d64a36054e5fb71dc74bb59f6733
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-28 11:41:00 +0500

    create PatientRow component

---
+++ src/components/patients/PatientRow/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr class="patient-row">
+    <td>Avatar</td>
+    <td>{{ patient.name }}</td>
+    <td>{{ patient.phone }}</td>
+  </tr>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+import { Patient } from '@/models/Patient.model';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: {
+    patient: [Patient, Object],
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
