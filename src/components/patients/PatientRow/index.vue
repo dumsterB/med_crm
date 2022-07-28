fe0a3432a40be829cc861790c8c648e8b41f844e
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-28 14:47:04 +0500

    update PatientRow

--- src/components/patients/PatientRow/index.vue
+++ src/components/patients/PatientRow/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <tr class="patient-row">
-    <td>Avatar</td>
-    <td>{{ patient.name }}</td>
+  <tr class="patient-row" @click="goToPatient">
+    <td>
+      <div class="patient-row__main">
+        <UiAvatar :img="patient.avatar" />
+        <span>{{ patient.name }}</span>
+      </div>
+    </td>
     <td>{{ patient.phone }}</td>
   </tr>
 </template>
@@ -9,11 +13,21 @@
 <script>
 import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
 import { Patient } from '@/models/Patient.model';
+import { REGISTRY_PATIENT_ROUTE } from '@/router/registry.routes';
 
 export default {
   name: getParentFolderNameByMetaUrl(import.meta.url),
   props: {
     patient: [Patient, Object],
+  },
+
+  methods: {
+    goToPatient() {
+      this.$router.push({
+        name: REGISTRY_PATIENT_ROUTE.name,
+        params: { id: this.patient.id },
+      });
+    },
   },
 };
 </script>
