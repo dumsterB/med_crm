b24e3631156b28a8ca99a1bcbee68ed8c64bccd5
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-28 18:10:43 +0500

    add default constants in api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,4 +1,7 @@
 export const DEFAULT_PER_PAGE = 12;
+export const DEFAULT_ORDER_DIRECTION = 'ASC';
+export const DEFAULT_ORDER_OPERATOR = 'OR';
+export const DEFAULT_QUERY_TYPE = 'ILIKE';
 
 // для объединений массива в строку в query запросов
 export const QUERY_ARRAY_SEPARATOR = ',';
