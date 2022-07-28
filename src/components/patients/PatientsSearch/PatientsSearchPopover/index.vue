7c5fcbd5761129682c2c1838eac61155318416d1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-28 11:40:04 +0500

    create PatientSearchPopover

---
+++ src/components/patients/PatientsSearch/PatientsSearchPopover/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="patients-search-popover">
+    <div v-show="noData" class="patients-search-popover__no-data">No data</div>
+
+    Popover
+  </div>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: {
+    search: String,
+    noData: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/i18n.locales.json" />
+<i18n src="./index.locales.json" />
