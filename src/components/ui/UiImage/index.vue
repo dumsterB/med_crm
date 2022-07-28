cb1b01bef199346aff086c8d7c4d745bc9fb0083
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-28 14:10:03 +0500

    base realized UiImage component

---
+++ src/components/ui/UiImage/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="['ui-image']">
+    <img
+      v-if="!error"
+      class="ui-image__image"
+      draggable="false"
+      loading="lazy"
+      :src="image?.src || image?.url"
+      :alt="image?.alt"
+      @error="errorHandler"
+      @load="loadHandler" />
+
+    <img
+      v-if="error"
+      class="ui-image__image"
+      src="@/images/assets/image-error.png"
+      :alt="image?.alt" />
+  </div>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: {
+    /**
+     * @param { Object } image
+     * @param { string } image.src
+     * @param { string } image.url  synonym for image.src
+     * @param { string } image.alt
+     */
+    image: Object,
+  },
+  data() {
+    return {
+      loading: true,
+      error: false,
+    };
+  },
+  watch: {
+    image: {
+      handler(value) {
+        if (!value || !value.src || !value.url) this.error = true;
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    loadHandler() {
+      this.loading = false;
+    },
+    errorHandler() {
+      this.loading = false;
+      this.error = true;
+    },
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
