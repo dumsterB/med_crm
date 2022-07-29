867ae7bb2c45cdffae25b491ca34332bc6407dd1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-29 16:50:06 +0500

    LayoutRegistry supported contentClass, mainClass props

--- src/components/layouts/LayoutRegistry/index.vue
+++ src/components/layouts/LayoutRegistry/index.vue
@@ -2,11 +2,11 @@
   <ElContainer class="layout-registry">
     <ElAside class="layout-registry__menu"> <RegistryMenu /> </ElAside>
 
-    <ElContainer>
+    <ElContainer :class="['layout-registry__main', mainClass]">
       <ElHeader class="layout-registry__header layout-registry__el-header">
         <RegistryHeader />
       </ElHeader>
-      <ElMain class="layout-registry__content"> <slot></slot> </ElMain>
+      <ElMain :class="['layout-registry__content', contentClass]"> <slot></slot> </ElMain>
     </ElContainer>
   </ElContainer>
 </template>
@@ -19,6 +19,10 @@ import RegistryMenu from '@/components/registry/RegistryMenu/index.vue';
 export default {
   name: getParentFolderNameByMetaUrl(import.meta.url),
   components: { RegistryHeader, RegistryMenu },
+  props: {
+    contentClass: String,
+    mainClass: String,
+  },
 };
 </script>
 
