a874154db545baf59a4eadfc5bec44d41f555f8a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-07-29 14:25:45 +0500

    create ui.config.js

---
+++ src/config/ui.config.js
@@ -0,0 +1,10 @@
+import { DEFAULT_PER_PAGE } from '@/config/api.config';
+
+export const PAGE_SIZE_STEP = DEFAULT_PER_PAGE;
+
+export const PAGE_SIZES = [
+  DEFAULT_PER_PAGE,
+  DEFAULT_PER_PAGE + PAGE_SIZE_STEP,
+  DEFAULT_PER_PAGE + PAGE_SIZE_STEP * 2,
+  DEFAULT_PER_PAGE + PAGE_SIZE_STEP * 3,
+];
