e3bf2521a15c860e2c6b2f0adaf455dce34bd3d2
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-01 17:44:06 +0500

    create empty CreatePatientDrawer

---
+++ src/components/patients/CreatePatientDrawer/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <ElDrawer
+    :model-value="modelValue"
+    title="Create Patient"
+    :direction="direction"
+    @update:model-value="$emit('update:modelValue', $event)">
+    Create Patient
+  </ElDrawer>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: {
+    modelValue: Boolean,
+    direction: String,
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="./index.locales.json" />
