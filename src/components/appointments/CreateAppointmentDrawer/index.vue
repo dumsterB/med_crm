96cc03bf7a95499b7eea1fd92c30cfa14053ef86
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-01 21:09:59 +0500

    create empty CreateAppointmentDrawer

---
+++ src/components/appointments/CreateAppointmentDrawer/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <ElDrawer
+    :model-value="modelValue"
+    title="Create appointment"
+    @update:model-value="$emit('update:modelValue', $event)">
+    <ElForm label-position="top" @submit.prevent="createAppointment">
+      <ElFormItem label="patient">
+        <ElSelect v-model="appointment.user_id">
+          <ElOption
+            v-for="user in [patient]"
+            :key="user.id"
+            :label="user.name"
+            :value="user.id"
+            disabled />
+        </ElSelect>
+      </ElFormItem>
+    </ElForm>
+  </ElDrawer>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+import { Patient } from '@/models/Patient.model';
+import { User } from '@/models/User.model';
+import { Appointment } from '@/models/Appointment.model';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: {
+    modelValue: Boolean,
+    patient: [Patient, User, Object],
+  },
+  data() {
+    return {
+      /**
+       * @type Appointment
+       */
+      appointment: null,
+    };
+  },
+  watch: {
+    modelValue: {
+      handler() {
+        this.appointment = new Appointment({ user_id: this.patient.id });
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    createAppointment() {},
+  },
+};
+</script>
