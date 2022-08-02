400a5e3f1cb84867c000bc791cb6511e3602b68d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-02 17:11:25 +0500

    create UiRequiredHiddenInput

---
+++ src/components/ui/UiRequiredHiddenInput/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <ElInput class="ui-required-hidden-input" :model-value="modelValue" required v-bind="$attrs" />
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: ['modelValue'],
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
