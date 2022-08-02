15583f48eb2ae6389b812c4a23279507da69a75b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-02 13:27:45 +0500

    create UiPhoneInput component

---
+++ src/components/ui/UiPhoneInput/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <ElInput
+    :model-value="phone"
+    type="number"
+    v-bind="$attrs"
+    @update:model-value="updateModelValue">
+    <template #prefix> + </template>
+  </ElInput>
+</template>
+
+<script>
+import { getParentFolderNameByMetaUrl } from '@/utils/vite.util';
+
+export default {
+  name: getParentFolderNameByMetaUrl(import.meta.url),
+  props: {
+    modelValue: String,
+  },
+  computed: {
+    phone() {
+      return this.modelValue?.replace(/^\+*/, '');
+    },
+  },
+
+  methods: {
+    updateModelValue(value) {
+      this.$emit('update:modelValue', `+${value}`.replace(/^\+*/, '+'));
+    },
+  },
+};
+</script>
