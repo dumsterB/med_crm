62b45b98374429758d14ff189da2f03d0ca9178f
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-04 12:17:30 +0500

    update CreatePatientDrawer

--- src/components/appointments/CreateAppointmentDrawer/index.vue
+++ src/components/appointments/CreateAppointmentDrawer/index.vue
@@ -1,19 +1,31 @@
 <template>
   <ElDrawer
     :model-value="modelValue"
-    title="Create appointment"
+    :title="$t('Title')"
     @update:model-value="$emit('update:modelValue', $event)">
     <ElForm label-position="top" @submit.prevent="createAppointment">
-      <ElFormItem label="patient">
-        <ElSelect v-model="appointment.user_id">
-          <ElOption
-            v-for="user in [patient]"
-            :key="user.id"
-            :label="user.name"
-            :value="user.id"
-            disabled />
-        </ElSelect>
-      </ElFormItem>
+      <ElTabs v-model="appointmentType">
+        <ElTabPane
+          :label="$t(`Appointments.Types.${appointmentTypesEnum.Doctor}`)"
+          :name="appointmentTypesEnum.Doctor">
+          <ElFormItem label="patient">
+            <ElSelect v-model="appointment.user_id">
+              <ElOption
+                v-for="user in [patient]"
+                :key="user.id"
+                :label="user.name"
+                :value="user.id"
+                disabled />
+            </ElSelect>
+          </ElFormItem>
+        </ElTabPane>
+
+        <ElTabPane
+          :label="$t(`Appointments.Types.${appointmentTypesEnum.Service}`)"
+          :name="appointmentTypesEnum.Service">
+          Service
+        </ElTabPane>
+      </ElTabs>
     </ElForm>
   </ElDrawer>
 </template>
@@ -25,6 +37,7 @@ import { Appointment } from '@/models/Appointment.model';
 
 export default {
   name: 'CreateAppointmentDrawer',
+  emits: ['update:modelValue', 'action'],
   props: {
     modelValue: Boolean,
     patient: [Patient, User, Object],
@@ -35,14 +48,22 @@ export default {
        * @type Appointment
        */
       appointment: null,
+      appointmentType: Appointment.enum.types.Doctor,
     };
   },
+  computed: {
+    appointmentTypesEnum: () => Appointment.enum.types,
+    watchersForResetAppointment() {
+      return [this.modelValue, this.appointmentType];
+    },
+  },
   watch: {
-    modelValue: {
+    watchersForResetAppointment: {
       handler() {
-        this.appointment = new Appointment({ user_id: this.patient.id });
+        this.appointment = new Appointment({ patient_id: this.patient.id });
       },
       immediate: true,
+      deep: true,
     },
   },
 
@@ -51,3 +72,8 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/base.locales.json" />
+<i18n src="@/locales/appointments.locales.json" />
+<i18n src="./index.locales.json" />
