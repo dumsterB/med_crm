aaa0cbdf48f346f64e3c5d7923d4f0aa6fbadb67
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-04 11:03:33 +0500

    update Appointment model

--- src/models/Appointment.model.js
+++ src/models/Appointment.model.js
@@ -12,10 +12,11 @@ export class Appointment extends CRUDModel {
    * @param {object} [payload]
    * @param {number} payload.id
    * @param {number} payload.doctor_id
-   * @param {number} payload.user_id
-   * @param {number} payload.patient_id synonym for payload.user_id
+   * @param {number} payload.patient_id
+   * @param {number} payload.user_id synonym for payload.patient_id
    * @param {Date|string} payload.start_at
    * @param {Date|string} payload.end_at
+   * @param {Speciality} payload.speciality
    * @param {string} payload.status
    * @param {string} payload.cancel_reason
    * @param {string} payload.cancel_description
@@ -24,9 +25,10 @@ export class Appointment extends CRUDModel {
     super(payload);
 
     this.doctor_id = payload?.doctor_id ?? null;
-    this.user_id = payload?.user_id ?? payload?.patient_id ?? null;
+    this.patient_id = payload?.patient_id ?? payload?.user_id ?? null;
     this.start_at = payload?.start_at ?? null;
     this.end_at = payload?.end_at ?? null;
+    this.speciality = payload?.speciality ?? null;
     this.status = payload?.status ?? null;
     this.cancel_reason = payload?.cancel_reason ?? null;
     this.cancel_description = payload?.cancel_description ?? null;
