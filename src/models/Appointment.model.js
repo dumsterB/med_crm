bf87034c7e10243065d4de238716ad358308626a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-05 16:18:05 +0500

    update Appointment

--- src/models/Appointment.model.js
+++ src/models/Appointment.model.js
@@ -11,12 +11,13 @@ export class Appointment extends CRUDModel {
   /**
    * @param {object} [payload]
    * @param {number} payload.id
-   * @param {number} payload.doctor_id
    * @param {number} payload.patient_id
+   * @param {number} payload.doctor_id
+   * @param {number} payload.specialty_id
+   * @param {number} payload.service_id
    * @param {number} payload.user_id synonym for payload.patient_id
    * @param {Date|string} payload.start_at
    * @param {Date|string} payload.end_at
-   * @param {Specialty} payload.speciality
    * @param {string} payload.status
    * @param {string} payload.cancel_reason
    * @param {string} payload.cancel_description
@@ -24,11 +25,12 @@ export class Appointment extends CRUDModel {
   constructor(payload) {
     super(payload);
 
-    this.doctor_id = payload?.doctor_id ?? null;
     this.patient_id = payload?.patient_id ?? payload?.user_id ?? null;
+    this.doctor_id = payload?.doctor_id ?? null;
+    this.specialty_id = payload?.specialty_id ?? null;
+    this.service_id = payload?.service_id ?? null;
     this.start_at = payload?.start_at ?? null;
     this.end_at = payload?.end_at ?? null;
-    this.specialty_id = payload?.speciality ?? null;
     this.status = payload?.status ?? null;
     this.cancel_reason = payload?.cancel_reason ?? null;
     this.cancel_description = payload?.cancel_description ?? null;
