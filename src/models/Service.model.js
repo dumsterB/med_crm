8c387863dca4fa0f26675d469ebee3f786a4f238
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-05 19:38:19 +0500

    create Service model

---
+++ src/models/Service.model.js
@@ -0,0 +1,56 @@
+import { CRUDModel } from '@/models/CRUD.model';
+
+/**
+ * @class Service
+ * @extends CRUDModel
+ */
+export class Service extends CRUDModel {
+  static modelName = 'service';
+  static tableName = 'services';
+
+  /**
+   *
+   * @param {object} [payload]
+   * @param {number} payload.price
+   * @param {string} payload.title
+   * @param {string} payload.description
+   * @param {String<Service.enum.types>} payload.type
+   * @param {number} payload.category_id
+   * @param {ServiceCategory|object} payload.category
+   * @param {number} payload.doctor_id
+   * @param {Doctor} payload.doctor
+   */
+  constructor(payload) {
+    super(payload);
+
+    this.price = payload?.price ?? null;
+    this.title = payload?.title ?? '';
+    this.description = payload?.description ?? null;
+    this.type = payload?.type ?? null;
+    this.category_id = payload?.category_id ?? null;
+    this.category = payload?.category ?? null;
+    this.doctor_id = payload?.doctor_id ?? null;
+    this.doctor = payload?.doctor ?? null;
+  }
+
+  static enum = {
+    types: {
+      Analysis: 'analysis',
+      Cosmetology: 'cosmetology',
+      Procedure: 'procedure',
+      Vaccine: 'vaccine',
+      Service: 'service',
+    },
+  };
+
+  /**
+   * @param {object} payload CRUDModel.find.params + doctorId
+   * @return {Promise<{response: AxiosResponse<*>, data: response.data}>}
+   */
+  static async find(payload) {
+    return super.find({
+      _url: `${this.tableName}/${payload.doctorId}`,
+      ...payload,
+    });
+  }
+}
