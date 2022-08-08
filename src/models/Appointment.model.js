943da1a63cc527bf6d3676b61010d06800e75051
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-08 14:28:38 +0500

    update Appointment

--- src/models/Appointment.model.js
+++ src/models/Appointment.model.js
@@ -15,6 +15,7 @@ export class Appointment extends CRUDModel {
    * @param {number} payload.doctor_id
    * @param {number} payload.specialty_id
    * @param {number} payload.service_id
+   * @param {number} payload.group_service_id
    * @param {number} payload.user_id synonym for payload.patient_id
    * @param {Date|string} payload.start_at
    * @param {Date|string} payload.end_at
@@ -29,11 +30,22 @@ export class Appointment extends CRUDModel {
     this.doctor_id = payload?.doctor_id ?? null;
     this.specialty_id = payload?.specialty_id ?? null;
     this.service_id = payload?.service_id ?? null;
+    this.group_service_id = payload?.group_service_id ?? null;
     this.start_at = payload?.start_at ?? null;
     this.end_at = payload?.end_at ?? null;
     this.status = payload?.status ?? null;
     this.cancel_reason = payload?.cancel_reason ?? null;
     this.cancel_description = payload?.cancel_description ?? null;
+  }
+
+  /**
+   * @param {object} payload
+   * @return {Promise<{data: response.data, response: AxiosResponse<*>}>}
+   */
+  static async create(payload) {
+    return super.create(payload, {
+      url: payload?.group_service_id ? `${this.tableName}/create/byGroup` : null,
+    });
   }
 
   static enum = {
