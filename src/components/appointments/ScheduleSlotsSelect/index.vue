4c299c1f4a1232222b45f131c5c50f62131478bc
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-08 14:31:23 +0500

    realized ScheduleSlotsSelect

--- src/components/appointments/ScheduleSlotsSelect/index.vue
+++ src/components/appointments/ScheduleSlotsSelect/index.vue
@@ -3,16 +3,30 @@
     <ElDatePicker
       class="schedule-slots-select__date"
       v-model="date"
+      placeholder="select date"
       :disabled="disabled"
       :value-format="DATE_FORMAT">
     </ElDatePicker>
-    <ElSelect class="schedule-slots-select__time" v-model="slot" :disabled="disabled"> </ElSelect>
+
+    <ElSelect
+      class="schedule-slots-select__time"
+      v-model="slot"
+      value-key="start_at"
+      :disabled="disabled"
+      :loading="loading">
+      <ElOption
+        v-for="slot in slotsForSelect"
+        :key="slot.label"
+        :label="slot.label"
+        :value="slot.value" />
+    </ElSelect>
   </div>
 </template>
 
 <script>
 import { DATE_FORMAT } from '@/config/dateAndTime.config';
 import { ScheduleSlot } from '@/models/ScheduleSlot.model';
+import { excludeDate } from '@/utils/dateAndTime.utils';
 
 export default {
   name: 'ScheduleSlotsSelect',
@@ -26,25 +40,53 @@ export default {
   },
   data() {
     return {
-      date: null,
-      slot: null,
       loading: false,
+      date: null,
+      slot: {
+        start_at: null,
+        end_at: null,
+      },
+      /** @type {Array<ScheduleSlot>} */
+      slots: [],
     };
   },
   computed: {
-    from() {
-      return `${this.date} 00:00`;
+    availableSlots() {
+      return this.slots.filter((elem) => elem.available);
     },
-    to() {
-      return `${this.date} 23:59`;
+
+    /** @type {Array<{label: string, value: {startAt: string, endAt: string}}>} */
+    slotsForSelect() {
+      return this.availableSlots.map((elem) => {
+        return {
+          label: `${excludeDate(elem.start_at)} - ${excludeDate(elem.end_at)}`,
+          value: {
+            start_at: elem.start_at,
+            end_at: elem.end_at,
+          },
+        };
+      });
     },
   },
   watch: {
     date: {
       handler(value) {
+        this.slot = null;
         if (value) this.getSlots();
       },
       immediate: true,
+    },
+
+    slot(value) {
+      if (!value) return;
+      this.$emit('update:startAt', value.start_at);
+      this.$emit('update:endAt', value.end_at);
+    },
+    startAt(value) {
+      if (this.slot.start_at !== value) this.slot.start_at = value;
+    },
+    endAt(value) {
+      if (this.slot.end_at !== value) this.slot.end_at = value;
     },
   },
 
@@ -58,10 +100,10 @@ export default {
     async getSlotsByService() {
       const { data } = await ScheduleSlot.getSlotsByServiceId({
         serviceId: this.serviceId,
-        from: this.from,
-        to: this.to,
+        from: `${this.date} 00:00`,
+        to: `${this.date} 23:59`,
       });
-      console.log(data);
+      this.slots = data.data;
     },
     async getSlotsByGroupService() {
       const { data } = await ScheduleSlot.getSlotsByGroupServiceId({
@@ -69,7 +111,6 @@ export default {
         from: this.from,
         to: this.to,
       });
-      console.log(data);
     },
   },
 
