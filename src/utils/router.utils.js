2bb98b24704e1a219e6d99e00653ca5f83d147b4
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-09 13:29:41 +0500

    update insertRouteParams

--- src/utils/router.utils.js
+++ src/utils/router.utils.js
@@ -1,9 +1,9 @@
 /**
- * @param {string} route vue-router Router.path
+ * @param {string} path vue-router Router.path
  * @param {object} params params for replace
  */
-export function insertRouteParams({ route, params }) {
-  return route.replace(/:[a-zA-Z]*/gim, (str) => {
+export function insertRouteParams({ path, params }) {
+  return path.replace(/:[a-zA-Z]*/gim, (str) => {
     const paramKey = str.split(':')[1];
     return params[paramKey];
   });
