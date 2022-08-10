6fe089e6f8c32eabe29561526499a53265c24534
Dumster <abduvalievbehr.uz1@gmail.com>
2022-08-10 21:55:24 +0500

    refactoring

--- src/views/registry/VPatientsRecords/index.vue
+++ src/views/registry/VPatientsRecords/index.vue
@@ -2,28 +2,28 @@
   <LayoutRegistry content-class="v-patients-records-content" fixHeight >
     <div class="v-patients-records-content__header v-patients-records-content-header">
       <div class="v-patients-records-content-header-filters">
-        <elSelect v-model="value" class="v-patients-records-content-header-filters__field" :placeholder="$t('Appointments.SelectDoctor')" size="large">
-          <elOption
+        <ElSelect v-model="value" class="v-patients-records-content-header-filters__field" :placeholder="$t('Appointments.SelectDoctor')" size="large">
+          <ElOption
               v-for="item in options"
               :key="item.value"
               :label="item.label"
               :value="item.value"
           />
-        </elSelect>
-        <elSelect v-model="value" class="v-patients-records-content-header-filters__field2" :placeholder="$t('Appointments.SelectStatus')" size="large">
-          <elOption
+        </ElSelect>
+        <ElSelect v-model="value" class="v-patients-records-content-header-filters__field2" :placeholder="$t('Appointments.SelectStatus')" size="large">
+          <ElOption
               v-for="item in options"
               :key="item.value"
               :label="item.label"
               :value="item.value"
           />
-        </elSelect>
+        </ElSelect>
       </div>
       <div class="v-patients-records-content-header-actions">
         <ElButton type="primary" @click="createRecord"> {{ $t('Patients.AddPatient') }} </ElButton>
       </div>
     </div>
-    <patientsRecordsTable
+    <PatientsRecordsTable
       class="v-patients-records-content__table"
       :items="items"
       v-model:page="page.value"
@@ -36,13 +36,13 @@
 <script>
 import { mapState, mapActions } from 'vuex';
 import LayoutRegistry from '@/components/layouts/LayoutRegistry/index.vue';
-import patientsRecordsTable from '@/components/patientsRecords/patientsRecordsTable/index.vue';
+import PatientsRecordsTable from '@/components/patientsRecords/patientsRecordsTable/index.vue';
 import { usePage, usePerPage, useSearch } from '@/hooks/query';
 import { Appointment } from '@/models/Appointment.model.js';
 import CreateOrEditAppointmentDrawer from "@/components/appointments/CreateOrEditAppointmentDrawer";
 export default {
   name: 'VPatientsRecords',
-  components: { LayoutRegistry, patientsRecordsTable },
+  components: { LayoutRegistry, PatientsRecordsTable },
 
   setup: () => ({
     perPage: usePerPage(),
