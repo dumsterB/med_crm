825a7ed826aeee8d8e5f754340d40d4b47c7ca28
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-11 19:41:42 +0500

    update VAppointment page

--- src/views/registry/VAppointment/index.vue
+++ src/views/registry/VAppointment/index.vue
@@ -1,15 +1,57 @@
 <template>
-  <LayoutRegistry content-class="v-patient-content"> PatientRecord </LayoutRegistry>
+  <LayoutRegistry content-class="v-patient-content">
+    PatientRecord
+    <ElButton type="primary" @click="editAppointment"> Edit </ElButton>
+  </LayoutRegistry>
 </template>
 
 <script>
+import { Appointment } from '@/models/Appointment.model';
+
 import LayoutRegistry from '@/components/layouts/LayoutRegistry/index.vue';
+import CreateOrEditAppointmentDrawer from '@/components/appointments/CreateOrEditAppointmentDrawer/index.vue';
 
 export default {
   name: 'VAppointment',
   components: { LayoutRegistry },
   props: {
     id: [Number, String],
+    patientId: [Number, String],
+  },
+  data() {
+    return {
+      loading: false,
+      appointment: null,
+    };
+  },
+  watch: {
+    id: {
+      handler() {
+        this.getAppointment();
+      },
+      immediate: true,
+    },
+  },
+
+  methods: {
+    async getAppointment() {
+      this.loading = true;
+
+      const { data } = await Appointment.findOneById(this.id);
+      this.appointment = data.data;
+
+      this.loading = false;
+    },
+
+    async editAppointment() {
+      const action = await this.$store.dispatch('modalAndDrawer/openDrawer', {
+        component: CreateOrEditAppointmentDrawer,
+        payload: {
+          data: this.appointment,
+        },
+      });
+      console.log(action);
+    },
   },
 };
 </script>
