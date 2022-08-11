5b595c630d8a7db1e920f4e7f194a81e34b70206
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-11 21:39:07 +0500

    update Appointment model

--- src/models/Appointment.model.js
+++ src/models/Appointment.model.js
@@ -12,10 +12,15 @@ export class Appointment extends CRUDModel {
    * @param {object} [payload]
    * @param {number} payload.id
    * @param {number} payload.patient_id
+   * @param {Patient} payload.patient
    * @param {number} payload.doctor_id
+   * @param {Doctor} payload.doctor
    * @param {number} payload.specialty_id
+   * @param {Specialty} payload.specialty
    * @param {number} payload.service_id
+   * @param {Service} payload.service
    * @param {number} payload.group_service_id
+   * @param {ServiceGroup} payload.group_service
    * @param {number} payload.user_id synonym for payload.patient_id
    * @param {Date|string} payload.start_at
    * @param {Date|string} payload.end_at
@@ -27,10 +32,15 @@ export class Appointment extends CRUDModel {
     super(payload);
 
     this.patient_id = payload?.patient_id ?? payload?.user_id ?? null;
+    this.patient = payload?.patient ?? null;
     this.doctor_id = payload?.doctor_id ?? null;
+    this.doctor = payload?.doctor ?? null;
     this.specialty_id = payload?.specialty_id ?? null;
+    this.specialty = payload?.specialty ?? null;
     this.service_id = payload?.service_id ?? null;
+    this.service = payload?.service ?? null;
     this.group_service_id = payload?.group_service_id ?? null;
+    this.group_service = payload?.group_service ?? null;
     this.start_at = payload?.start_at ?? null;
     this.end_at = payload?.end_at ?? null;
     this.status = payload?.status ?? null;
@@ -53,10 +63,10 @@ export class Appointment extends CRUDModel {
       Doctor: 'doctor',
       Service: 'service',
     },
-    statuses:{
+    statuses: {
       Approved: 'approved',
       Canceled: 'canceled',
-      Provided: 'provided'
-    }
+      Provided: 'provided',
+    },
   };
 }
