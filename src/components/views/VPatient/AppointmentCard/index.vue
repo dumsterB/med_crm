a7700159b1eb3e38aa78ce16517f33e7e4314f1d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-15 16:17:34 +0500

    feature/MIS-51_front update VPatient/AppointmentCard

--- src/components/views/VPatient/AppointmentCard/index.vue
+++ src/components/views/VPatient/AppointmentCard/index.vue
@@ -1,60 +1,62 @@
-<template #header>
-  <ElCard
-    class="v-patient-appointment-card"
-    v-for="appointment of data"
-    #header
-    :key="appointment.id">
-
-    <div class="v-patient-appointment-content-profile">
-      <UiAvatar size="super-large" class="v-patient-appointment-profile__avatar" :img="appointment.doctor.avatar.link" />
-      <div class="v-patient-appointment-content-profile-info">
-        <ElTag class="v-patient-appointment-content-profile__danger-button">
-          {{ $t('Appointment') }}
-        </ElTag>
-        <p class="v-patient-appointment-content-profile__title">
-          {{ appointment.doctor.name }}
-        </p>
-        <p
-          class="v-patients-appointments-content_profile__text"
-          v-for="specialties of appointment.doctor.specialties"
-          :key="specialties">
-          {{ specialties.title }}
-        </p>
+<template>
+  <ElCard class="v-patient-appointment-card" v-for="appointment of data" :key="appointment.id">
+    <template #header>
+      <div class="v-patient-appointment-content-profile">
+        <UiAvatar
+          size="super-large"
+          class="v-patient-appointment-profile__avatar"
+          :image="{ src: appointment.doctor.avatar.link, alt: 'avatar' }" />
+        <div class="v-patient-appointment-content-profile-info">
+          <ElTag class="v-patient-appointment-content-profile__danger-button">
+            {{ $t('Appointment') }}
+          </ElTag>
+          <p class="v-patient-appointment-content-profile__title">
+            {{ appointment.doctor.name }}
+          </p>
+          <p
+            class="v-patients-appointments-content-profile__text"
+            v-for="specialties of appointment.doctor.specialties"
+            :key="specialties">
+            {{ specialties.title }}
+          </p>
+        </div>
       </div>
-    </div>
-
-    <ElDivider class="v-patient-appointment-content__divider" />
+    </template>
 
     <div class="v-patient-appointment-content-info">
-        <p class="v-patient-appointment-content-info__title">
-          {{ $t('DateAndTine') }}
-        </p>
-        <p class="v-patients-appointments-content-info__text">
-          {{appointment.start_at}}
-        </p>
-        <p class="v-patient-appointment-content-info__title">
-          {{ $t('Price') }}
-        </p>
-        <p class="v-patient-appointment-content-info__text">
-          {{  appointment.service.price }} сум
-        </p>
+      <p class="v-patient-appointment-content-info__title">
+        {{ $t('DateAndTine') }}
+      </p>
+      <p class="v-patients-appointments-content-info__text">
+        {{ appointment.start_at }}
+      </p>
+      <p class="v-patient-appointment-content-info__title">
+        {{ $t('Price') }}
+      </p>
+      <p class="v-patient-appointment-content-info__text">{{ appointmentPrice }} сум</p>
     </div>
-
   </ElCard>
 </template>
 
 <script>
 import * as icons from '@/enums/icons.enum';
 import { Appointment } from '@/models/Appointment.model';
+import { PriceService } from '@/services/price.service';
+
 export default {
   name: 'VPatientAppointmentCard',
   props: {
     data: {
-      type: Array,
+      type: [Appointment, Object],
       required: true,
     },
   },
   icons: icons,
+  computed: {
+    appointmentPrice() {
+      return PriceService.formatPrice(this.data.service?.price);
+    },
+  },
 };
 </script>
 
