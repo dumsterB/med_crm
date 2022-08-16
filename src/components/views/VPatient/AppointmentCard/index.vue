6777dbedb0eec3ebe5d303e8b69fa91a23b30082
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-16 10:57:57 +0500

    MIS-51-front refactored VPatient/AppointmentCard

--- src/components/views/VPatient/AppointmentCard/index.vue
+++ src/components/views/VPatient/AppointmentCard/index.vue
@@ -1,39 +1,26 @@
 <template>
-  <ElCard class="v-patient-appointment-card" v-for="appointment of data" :key="appointment.id">
+  <ElCard class="v-patient-appointment-card" shadow="hover" @click="goToAppointment">
     <template #header>
-      <div class="v-patient-appointment-content-profile">
-        <UiAvatar
-          size="super-large"
-          class="v-patient-appointment-profile__avatar"
-          :image="{ src: appointment.doctor.avatar.link, alt: 'avatar' }" />
-        <div class="v-patient-appointment-content-profile-info">
-          <ElTag class="v-patient-appointment-content-profile__danger-button">
-            {{ $t('Appointment') }}
-          </ElTag>
-          <p class="v-patient-appointment-content-profile__title">
-            {{ appointment.doctor.name }}
-          </p>
-          <p
-            class="v-patients-appointments-content-profile__text"
-            v-for="specialties of appointment.doctor.specialties"
-            :key="specialties">
-            {{ specialties.title }}
-          </p>
+      <div class="v-patient-appointment-card-header">
+        <UiAvatar size="super-large" :image="{ src: data.doctor.avatar.link, alt: 'avatar' }" />
+
+        <div class="v-patient-appointment-card__doctor v-patient-appointment-card-doctor">
+          <div class="v-patient-appointment-card-doctor__name">
+            {{ data.doctor.name }}
+          </div>
+
+          <div class="v-patient-appointment-card-doctor__specialties">
+            <span v-for="(specialties, index) of data.doctor.specialties" :key="specialties">
+              {{ index !== 0 ? ', ' : '' }}{{ specialties.title }}
+            </span>
+          </div>
         </div>
       </div>
     </template>
 
-    <div class="v-patient-appointment-content-info">
-      <p class="v-patient-appointment-content-info__title">
-        {{ $t('DateAndTine') }}
-      </p>
-      <p class="v-patients-appointments-content-info__text">
-        {{ appointment.start_at }}
-      </p>
-      <p class="v-patient-appointment-content-info__title">
-        {{ $t('Price') }}
-      </p>
-      <p class="v-patient-appointment-content-info__text">{{ appointment.service.price }} {{ $t('Base.Sum') }}</p>
+    <div class="v-patient-appointment-card-data" v-for="item in infoItems" :key="item.value">
+      <div class="v-patient-appointment-card-data__title">{{ item.label }}</div>
+      <div class="v-patient-appointment-card-data_-value">{{ item.value }}</div>
     </div>
   </ElCard>
 </template>
@@ -42,19 +29,44 @@
 import * as icons from '@/enums/icons.enum';
 import { Appointment } from '@/models/Appointment.model';
 import { PriceService } from '@/services/price.service';
+import { REGISTRY_APPOINTMENT_ROUTE } from '@/router/registry.routes';
 
 export default {
   name: 'VPatientAppointmentCard',
+  icons: icons,
   props: {
     data: {
       type: [Appointment, Object],
       required: true,
     },
   },
-  icons: icons,
   computed: {
     appointmentPrice() {
-      return PriceService.formatPrice( 1000 );
+      return PriceService.formatPrice({ price: this.data.service?.price || 0 });
+    },
+    infoItems() {
+      return [
+        {
+          label: this.$t('Appointments.StartDateAndTime'),
+          value: this.data.start_at,
+        },
+        {
+          label: this.$t('Base.Price'),
+          value: this.appointmentPrice,
+        },
+      ];
+    },
+  },
+
+  methods: {
+    goToAppointment() {
+      this.$router.push({
+        ...REGISTRY_APPOINTMENT_ROUTE,
+        params: {
+          patientId: this.data.patient_id,
+          id: this.data.id,
+        },
+      });
     },
   },
 };
@@ -62,6 +74,5 @@ export default {
 
 <style lang="scss" src="./index.scss" />
 <i18n src="@/locales/base.locales.json" />
-<i18n src="@/locales/user.locales.json" />
 <i18n src="@/locales/appointments.locales.json" />
 <i18n src="./index.locales.json" />
