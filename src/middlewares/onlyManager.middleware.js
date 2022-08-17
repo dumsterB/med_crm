0f66f29bdb334dbf70fd7aa55c77153814a9cfec
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-17 14:36:04 +0500

    fix onlyManager.middleware.js

--- src/middlewares/onlyManager.middleware.js
+++ src/middlewares/onlyManager.middleware.js
@@ -3,6 +3,5 @@ import { User } from '@/models/User.model';
 
 export const onlyManagerMiddleware = function (from, to, next) {
   const isManager = Store.state.auth.user.role === User.enum.roles.Manager;
-
-  return next(isManager ? '' : '/');
+  return next(isManager ? null : '/');
 };
