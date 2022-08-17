babcd0031f17f0000a66ac9a6e4dc79f89ce4bf3
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-17 10:33:14 +0500

    create LayoutDefault

---
+++ src/components/layouts/LayoutDefault/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <ElContainer class="layout-default">
+    <ElAside class="layout-default__menu">
+      <slot name="menu"> </slot>
+    </ElAside>
+
+    <ElContainer
+      :class="[
+        'layout-default__main',
+        { 'layout-default__main_fix-height': fixHeight },
+        mainClass,
+      ]">
+      <ElHeader class="layout-default__header layout-default__el-header">
+        <slot name="header"> </slot>
+      </ElHeader>
+
+      <ElMain :class="['layout-default__content', contentClass]" v-loading="loading">
+        <slot></slot>
+      </ElMain>
+    </ElContainer>
+  </ElContainer>
+</template>
+
+<script>
+export default {
+  name: 'LayoutDefault',
+  props: {
+    fixHeight: Boolean,
+    contentClass: String,
+    mainClass: String,
+    loading: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
