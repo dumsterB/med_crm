056d07ecb00da9e07d57dd63d3ae7b84c6ad2119
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-17 10:59:06 +0500

    use DefaultMenu in RegistryMenu

--- src/components/registry/RegistryMenu/index.vue
+++ src/components/registry/RegistryMenu/index.vue
@@ -1,28 +1,12 @@
 <template>
-  <div class="registry-menu">
-    <img class="registry-menu__logo" src="@/assets/images/logo.svg" alt="logo" />
-
-    <nav class="registry-menu__nav registry-menu-nav">
-      <router-link
-        class="registry-menu-nav__item"
-        v-for="route in navigation"
-        :key="route.path"
-        :to="route.path">
+  <DefaultMenu>
+    <template #nav>
+      <router-link v-for="route in navigation" :key="route.path" :to="route.path">
         <UiIcon :icon="route.icon" />
         <span>{{ route.title }}</span>
       </router-link>
-    </nav>
-
-    <div class="registry-menu__actions registry-menu-actions">
-      <ElButton link @click="logout">
-        <template #icon>
-          <UiIcon :icon="$options.icons.LOGOUT" />
-        </template>
-
-        {{ $t('Base.Logout') }}
-      </ElButton>
-    </div>
-  </div>
+    </template>
+  </DefaultMenu>
 </template>
 
 <script>
@@ -32,16 +16,11 @@ import {
   REGISTRY_PATIENTS_ROUTE,
   REGISTRY_APPOINTMENTS_ROUTE,
 } from '@/router/registry.routes';
+import DefaultMenu from '@/components/DefaultMenu/index.vue';
 
 export default {
   name: 'RegistryMenu',
-  icons: icons,
-  methods: {
-    logout() {
-      this.$store.dispatch('auth/logout');
-    },
-  },
-
+  components: { DefaultMenu },
   computed: {
     navigation() {
       return [REGISTRY_DASHBOARD_ROUTE, REGISTRY_PATIENTS_ROUTE, REGISTRY_APPOINTMENTS_ROUTE].map(
