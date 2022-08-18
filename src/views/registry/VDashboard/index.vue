071bff42e69f164afa72c97cd01f62bdc50bd83c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-18 20:37:38 +0500

    MIS-55_calendar use EventCalendar in VDashboard

--- src/views/registry/VDashboard/index.vue
+++ src/views/registry/VDashboard/index.vue
@@ -1,17 +1,47 @@
 <template>
   <LayoutRegistry>
-    <router-link to="/auth/login">Login</router-link>
+    <EventCalendar v-model:type="type.value" v-model:date="date.value">
+      <template #actions>
+        <UiModelsAutocompleteSearch
+          v-model="doctorId"
+          :model-for-use="Doctor"
+          :placeholder="$t('Appointments.SelectDoctor')" />
+      </template>
+    </EventCalendar>
   </LayoutRegistry>
 </template>
 
 <script>
 import LayoutRegistry from '@/components/layouts/LayoutRegistry/index.vue';
+import EventCalendar from '@/components/EventCalendar/index.vue';
+import { EVENT_CALENDAR_TYPES } from '@/components/EventCalendar/index.enum';
+import { useQuery } from '@/hooks/useQuery.hook';
+import { Doctor } from '@/models/Doctor.model';
 
 export default {
   name: 'VDashboard',
-  components: { LayoutRegistry },
+  components: { EventCalendar, LayoutRegistry },
+  data() {
+    return {
+      doctorId: null,
+    };
+  },
+
+  setup: () => ({
+    date: useQuery({
+      field: 'calendar_date',
+      defaultValue: new Date().toISOString(),
+      formatter: (val) => new Date(val).toISOString(),
+    }),
+    type: useQuery({
+      field: 'calendar_type',
+      defaultValue: EVENT_CALENDAR_TYPES.MONTH,
+    }),
+    Doctor: Doctor,
+  }),
 };
 </script>
 
 <style lang="scss" src="./index.scss" />
+<i18n src="@/locales/appointments.locales.json" />
 <i18n src="./index.locales.json" />
