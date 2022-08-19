07b36190adc140b2f52643c2c5a548476792f51a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-19 17:39:07 +0500

    MIS-55_calendar update EventCalendar/DayCalendar

--- src/components/EventCalendar/DayCalendar/index.vue
+++ src/components/EventCalendar/DayCalendar/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="ec-day-calendar">Day calendar</div>
+  <div class="ec-day-calendar">
+    <div class="ec-day-calendar__header ec-day-calendar-header">
+      <div class="ec-day-calendar-header__prev">prev</div>
+
+      <div class="ec-day-calendar__current-day">
+        {{}}
+      </div>
+
+      <div class="ec-day-calendar-header__next">next</div>
+    </div>
+
+    <div class="ec-day-calendar__content ec-day-calendar-content"></div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'DayCalendar',
+  emits: ['update:date'],
+  props: {
+    date: String, // ISO format
+  },
 };
 </script>
 
