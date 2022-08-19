62ac292ffcaf589ec0377d5378df13a5e0344cd1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-19 17:55:02 +0500

    MIS-55_calendar base realized VDashboard

--- src/views/registry/VDashboard/index.vue
+++ src/views/registry/VDashboard/index.vue
@@ -1,10 +1,15 @@
 <template>
   <LayoutRegistry>
-    <EventCalendar v-model:type="type.value" v-model:date="date.value">
+    <EventCalendar
+      v-model:type="type.value"
+      v-model:date="date.value"
+      :loading="loading.calendar"
+      :month-data="dataForMonth">
       <template #actions>
         <UiModelsAutocompleteSearch
-          v-model="doctorId"
+          v-model="doctorId.value"
           :model-for-use="Doctor"
+          :default-item="doctor"
           :placeholder="$t('Appointments.SelectDoctor')" />
       </template>
     </EventCalendar>
@@ -17,26 +22,114 @@ import EventCalendar from '@/components/EventCalendar/index.vue';
 import { EVENT_CALENDAR_TYPES } from '@/components/EventCalendar/index.enum';
 import { useQuery } from '@/hooks/useQuery.hook';
 import { Doctor } from '@/models/Doctor.model';
-import { resetDaysInISOString, resetTimeInISOString } from '@/utils/dateAndTime.utils';
+import { Appointment } from '@/models/Appointment.model';
+import {
+  ISOStringToDateAppFormat,
+  getDaysInMonth,
+  resetDaysInISOString,
+  dateAppFormatToISOString,
+} from '@/utils/dateAndTime.utils';
 
 export default {
   name: 'VDashboard',
   components: { EventCalendar, LayoutRegistry },
   data() {
     return {
-      doctorId: null,
+      loading: {
+        calendar: false,
+        doctor: false,
+      },
+      dataForMonth: {},
+
+      /** @type Doctor */
+      doctor: null, // загрузить доктора при открытии страницы по ид из query
     };
+  },
+  computed: {
+    startAt() {
+      return resetDaysInISOString(this.date.value);
+    },
+    endAt() {
+      return this.startAt.replace(
+        /(\d\d\d\d)-(\d\d)-(\d\d)T/,
+        (str, year, month) => `${year}-${month}-${this.daysInMonth}T`
+      );
+    },
+    daysInMonth() {
+      return getDaysInMonth(this.startAt);
+    },
+
+    watchers() {
+      return {
+        type: this.type.value,
+        date: this.date.value,
+        doctorId: this.doctorId,
+      };
+    },
+  },
+
+  watch: {
+    watchers: {
+      async handler() {
+        this.loading.calendar = true;
+
+        this.type.value === EVENT_CALENDAR_TYPES.MONTH
+          ? await this.getDataForMonthType()
+          : await this.getDateForDayType();
+
+        this.loading.calendar = false;
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
+
+  methods: {
+    async getDataForMonthType() {
+      const { data } = await Appointment.getStatistic({
+        startAt: ISOStringToDateAppFormat(this.startAt, { withTime: false, fullYear: false }),
+        endAt: ISOStringToDateAppFormat(this.endAt, { withTime: false, fullYear: false }),
+        doctorsId: this.doctorId.value ? [this.doctorId.value] : [],
+      });
+
+      this.dataForMonth = {};
+      Object.keys(data.data).forEach((date) => {
+        this.dataForMonth[dateAppFormatToISOString(date).split('T')[0]] = [
+          { title: `${data.data[date].count} records` },
+        ];
+      });
+    },
+
+    async getDateForDayType() {},
+
+    async getDefaultDoctor() {
+      this.loading.doctor = true;
+
+      // const { data } = await Doctor.findOneById(this.doctorId);
+      // this.doctor = data.data;
+
+      this.loading.doctor = false;
+    },
+  },
+
+  mounted() {
+    if (this.doctorId.value) this.getDefaultDoctor();
   },
 
   setup: () => ({
+    type: useQuery({
+      field: 'calendar_type',
+      defaultValue: EVENT_CALENDAR_TYPES.MONTH,
+    }),
     date: useQuery({
       field: 'calendar_date',
       defaultValue: new Date().toISOString(),
       formatter: (val) => new Date(val).toISOString(),
     }),
-    type: useQuery({
-      field: 'calendar_type',
-      defaultValue: EVENT_CALENDAR_TYPES.MONTH,
+    doctorId: useQuery({
+      field: 'doctor_id',
+      defaultValue: null,
+      valueIsNumber: true,
     }),
     Doctor: Doctor,
   }),
