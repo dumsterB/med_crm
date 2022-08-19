42f13473d9d80d5cdb3d27b11900ae6fb8baa306
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-19 20:23:54 +0500

    MIS-55_calendar useQuery supported "routerPush param"

--- src/hooks/useQuery.hook.js
+++ src/hooks/useQuery.hook.js
@@ -6,16 +6,24 @@ import { useRouter, useRoute } from 'vue-router';
  * @param { number|string|null } [defaultValue]
  * @param { void|null } [formatter]
  * @param { boolean } [valueIsNumber = false]
+ * @param { boolean } [routerPush = true]
  * @return {string|LocationQueryValue[]|LocationQuery|UnwrapNestedRefs<{reset: reset, value: WritableComputedRef<string|LocationQueryValue[]|LocationQuery>}>|void|Promise<NavigationFailure | void | undefined>}
  */
-export function useQuery({ field, defaultValue = null, formatter = null, valueIsNumber = false }) {
+export function useQuery({
+  field,
+  defaultValue = null,
+  formatter = null,
+  valueIsNumber = false,
+  routerPush = false, // use router.replace or router.push
+}) {
   const router = useRouter();
   const route = useRoute();
+  const updateRoute = routerPush ? router.push : router.replace;
 
   const reset = function () {
     const query = { ...route.query };
     delete query[field];
-    router.push({ ...route, query });
+    updateRoute({ ...route, query });
   };
 
   const value = computed({
@@ -24,21 +32,21 @@ export function useQuery({ field, defaultValue = null, formatter = null, valueIs
       return valueIsNumber ? +value : value;
     },
     set(value) {
-      if (!field) {
-        router.push({
-          ...route,
-          query: { ...value },
-        });
-      }
-
       if (field) {
         value = formatter ? formatter(value) : value;
         value = value?.toString();
         if (!value && !value?.length) return reset();
 
-        router.push({
+        updateRoute({
           ...route,
           query: { ...route.query, [field]: value },
+        });
+      }
+
+      if (!field) {
+        updateRoute({
+          ...route,
+          query: { ...value },
         });
       }
     },
