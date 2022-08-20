08d58b441581f18293ac5b1f512dd8dc8e29ef59
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-20 14:02:05 +0500

    MIS-55_calendar update DayCalendar header

--- src/components/EventCalendar/DayCalendar/index.vue
+++ src/components/EventCalendar/DayCalendar/index.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="ec-day-calendar">
     <div class="ec-day-calendar__header ec-day-calendar-header">
-      <div class="ec-day-calendar-header__prev">prev</div>
+      <ElButton class="ec-day-calendar-header__prev" text>
+        <UiIcon :icon="icons.CHEVRON_LEFT" />
+      </ElButton>
 
       <div class="ec-day-calendar__current-day">
-        {{}}
+        {{ dateText }}
       </div>
 
-      <div class="ec-day-calendar-header__next">next</div>
+      <ElButton class="ec-day-calendar-header__next" text>
+        <UiIcon :icon="icons.CHEVRON_RIGHT" />
+      </ElButton>
     </div>
 
     <div class="ec-day-calendar__content ec-day-calendar-content"></div>
@@ -15,13 +19,33 @@
 </template>
 
 <script>
+import * as icons from '@/enums/icons.enum.js';
+
 export default {
   name: 'DayCalendar',
   emits: ['update:date'],
   props: {
     date: String, // ISO format
   },
+
+  computed: {
+    dateText() {
+      const date = new Date(this.date);
+      const day = date.getDate().toString();
+      const dayWeek = date.getDay().toString();
+      const month = date.getMonth().toString();
+
+      return `${day} ${this.$t(`DateAndTime.Months.${month}.Full`)}, ${this.$t(
+        `DateAndTime.Days.${dayWeek}.Full`
+      )}`;
+    },
+  },
+
+  setup: () => ({
+    icons,
+  }),
 };
 </script>
 
 <style lang="scss" src="./index.scss" />
+<i18n src="@/locales/dateAndTime.locales.json" />
