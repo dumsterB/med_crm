74e7285ae7d7fb94acf76c027218a4dcb25d68be
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-21 19:31:28 +0500

    create AppointmentsStartOrEndDate component

---
+++ src/components/appointments/AppointmentStartOrEndDate/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="date" class="appointments-start-or-end-date__text">{{ date }}</div>
+  <ElTag v-if="!date" type="warning" effect="dark" round>
+    {{ $t('Appointments.LiveQueue') }}
+  </ElTag>
+</template>
+
+<script>
+export default {
+  name: 'AppointmentStartOrEndDate',
+  props: {
+    date: String, // APP format
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/appointments.locales.json" />
