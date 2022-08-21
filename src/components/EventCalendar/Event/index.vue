d84c5ab4d7296fef62156072760d827d404e9bc2
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-21 12:32:01 +0500

    MIS-55_calendar update Event

--- src/components/EventCalendar/Event/index.vue
+++ src/components/EventCalendar/Event/index.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="ec-event">
-    <div class="ec-event__title">{{ data.title }}</div>
+    <div v-show="event.startAt && event.endAt" class="ec-event__time">
+      <slot name="time"> {{ time }} </slot>
+    </div>
+
+    <div v-show="event.title" class="ec-event__title">
+      <slot name="title">
+        {{ event.title }}
+      </slot>
+    </div>
+
+    <div v-show="event.description" class="ec-event__description">
+      <slot name="description">
+        {{ event.description }}
+      </slot>
+    </div>
   </div>
 </template>
 
 <script>
 import { EventCalendarEvent } from '@/components/EventCalendar/Event/EventCalendarEvent.model';
+import { excludeDate } from '@/utils/dateAndTime.utils';
 
 export default {
   name: 'EventCalendarEvent',
   props: {
     event: [EventCalendarEvent, Object],
+    shortTime: Boolean, // показывать только время
+  },
+
+  computed: {
+    time() {
+      return this.shortTime
+        ? `${excludeDate(this.event.startAt)} - ${excludeDate(this.event.endAt)}`
+        : `${this.event.startAt} - ${this.event.endAt}`;
+    },
   },
 };
 </script>
