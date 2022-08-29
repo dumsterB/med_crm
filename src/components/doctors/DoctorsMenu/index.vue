4314b77a88fbd0543d88aed5eca14b0f8b0840f1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-08-29 19:11:19 +0500

    MIS-126_templates add link to DOCTORS_SETTINGS_ROUTE in DoctorsMenu

--- src/components/doctors/DoctorsMenu/index.vue
+++ src/components/doctors/DoctorsMenu/index.vue
@@ -5,7 +5,7 @@
 <script>
 import * as icons from '@/enums/icons.enum.js';
 import { DASHBOARD_ROUTE } from '@/router/dashboard.routes';
-import { DOCTORS_QUEUE_ROUTE } from '@/router/doctors.routes';
+import { DOCTORS_QUEUE_ROUTE, DOCTORS_SETTINGS_ROUTE } from '@/router/doctors.routes';
 import DefaultMenu from '@/components/DefaultMenu/index.vue';
 
 export default {
@@ -13,7 +13,7 @@ export default {
   components: { DefaultMenu },
   computed: {
     navigation() {
-      return [DASHBOARD_ROUTE, DOCTORS_QUEUE_ROUTE].map((route) => ({
+      return [DASHBOARD_ROUTE, DOCTORS_QUEUE_ROUTE, DOCTORS_SETTINGS_ROUTE].map((route) => ({
         path: route.path,
         title: this.routesOptionsByRouteName[route.name].text,
         icon: this.routesOptionsByRouteName[route.name].icon,
@@ -29,6 +29,10 @@ export default {
         [DOCTORS_QUEUE_ROUTE.name]: {
           icon: icons.NOTE,
           text: this.$t('Base.Queue'),
+        },
+        [DOCTORS_SETTINGS_ROUTE.name]: {
+          icon: icons.NOTE,
+          text: this.$t('Base.Settings'),
         },
       };
     },
