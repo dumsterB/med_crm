7cf19dc3e4e9ab9a991103db854e5cdd496da20a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-04 14:14:22 +0500

    MIS-145_appoint use RouterView in VAppointment

--- src/views/appointments/VAppointment/index.vue
+++ src/views/appointments/VAppointment/index.vue
@@ -1,9 +1,6 @@
 <template>
   <LayoutByUserRole content-class="v-appointment-content" :loading="loading">
-    <AppointmentCard
-      class="v-appointment-content__card"
-      v-if="appointment"
-      v-model:data="appointment" />
+    <RouterView v-model:appointment="appointment"> </RouterView>
   </LayoutByUserRole>
 </template>
 
@@ -11,13 +8,12 @@
 import { Appointment } from '@/models/Appointment.model';
 import { GlobalDrawerCloseAction } from '@/models/client/ModalAndDrawer/GlobalDrawerCloseAction';
 
-import AppointmentCard from '@/components/views/VAppointment/AppointmentCard/index.vue';
 import CreateOrEditAppointmentDrawer from '@/components/appointments/CreateOrEditAppointmentDrawer/index.vue';
 import LayoutByUserRole from '@/components/layouts/LayoutByUserRole/index.vue';
 
 export default {
   name: 'VAppointment',
-  components: { LayoutByUserRole, AppointmentCard },
+  components: { LayoutByUserRole },
   props: {
     id: [Number, String],
     patientId: [Number, String],
@@ -25,7 +21,7 @@ export default {
   data() {
     return {
       loading: false,
-      appointment: null,
+      appointment: new Appointment(),
     };
   },
   watch: {
