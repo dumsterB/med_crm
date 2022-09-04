dd21dc35c88a382e214adc150dcf0c10c111edb0
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-04 13:30:48 +0500

    MIS-145_appoint create empty VAppointmentInspectionCard

---
+++ src/views/appointments/VAppointment/children/VAppointmentInspectionCard/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Inspection Card</div>
+</template>
+
+<script>
+import { Appointment } from '@/models/Appointment.model';
+
+export default {
+  name: 'VAppointmentInspectionCard',
+  props: {
+    appointment: [Appointment, Object],
+  },
+};
+</script>
