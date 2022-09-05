cbbd0f8f2e6876aa4c163d9488dc35a0c50bbb14
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-05 10:54:17 +0500

    MIS-145_appoint update VAppointmentInspectionCard

--- src/views/appointments/VAppointment/children/VAppointmentInspectionCard/index.vue
+++ src/views/appointments/VAppointment/children/VAppointmentInspectionCard/index.vue
@@ -1,14 +1,70 @@
 <template>
-  <div>Inspection Card</div>
+  <div>
+    <LayoutContentHeader>
+      <div class="v-app-ins-card-header">
+        <RouterLink :to="appointmentDefaultCardPageLink">
+          <ElButton text>
+            <template #icon>
+              <UiIcon :icon="icons.CHEVRON_LEFT" />
+            </template>
+            {{ $t('Base.Back') }}
+          </ElButton>
+        </RouterLink>
+
+        <div class="v-app-ins-card-header-info">
+          <span>{{ $t('Base.Inspection') }}:</span>
+          <RouterLink :to="patientPageLink">
+            <ElButton type="primary" text>
+              {{ appointment.patient?.name }}
+            </ElButton>
+          </RouterLink>
+        </div>
+      </div>
+    </LayoutContentHeader>
+  </div>
 </template>
 
 <script>
+import * as icons from '@/enums/icons.enum.js';
+import { APPOINTMENT_ROUTE } from '@/router/appointments.routes';
+import { PATIENT_ROUTE } from '@/router/patients.routes';
+import { insertRouteParams } from '@/utils/router.utils';
 import { Appointment } from '@/models/Appointment.model';
+
+import LayoutContentHeader from '@/components/layouts/assets/LayoutContentHeader/index.vue';
 
 export default {
   name: 'VAppointmentInspectionCard',
+  components: { LayoutContentHeader },
   props: {
     appointment: [Appointment, Object],
   },
+  computed: {
+    appointmentDefaultCardPageLink() {
+      return insertRouteParams({
+        path: APPOINTMENT_ROUTE.childrenMap.APPOINTMENT_ROUTE_DEFAULT_CARD._fullPath,
+        params: {
+          id: this.appointment.id,
+        },
+      });
+    },
+
+    patientPageLink() {
+      return insertRouteParams({
+        path: PATIENT_ROUTE.path,
+        params: {
+          id: this.appointment.patient_id,
+        },
+      });
+    },
+  },
+
+  setup: () => ({
+    icons,
+  }),
 };
 </script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/base.locales.json" />
+<i18n src="./index.locales.json" />
