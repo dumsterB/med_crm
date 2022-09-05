4bb5ba8a3d98a5e35ba42ef8376e7c6237a805ac
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-05 12:45:54 +0500

    MIS-151_ambulatory-card update User.model.js jsdocs comments

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -4,17 +4,33 @@ export class User extends CRUDModel {
   static modelName = 'user';
   static tableName = 'users';
 
+  /**
+   * @param {object} [payload]
+   * @param {number} payload.id
+   * @param {string} payload.name
+   * @param {string} payload.email
+   * @param {string} payload.phone
+   * @param {string} payload.birthdate
+   * @param {string} payload.gender from User.enum.genders
+   * @param {string} payload.role from User.enum.roles
+   * @param {number} payload.doctor_id
+   * @param {Doctor} payload.doctor
+   * @param {number} payload.childrens_count
+   * @param {Array<User|Patient>} payload.childrens
+   * @param {number} payload.parent_id
+   * @param {User|Patient} payload.parent
+   */
   constructor(payload) {
     super(payload);
+
     this.name = payload?.name ?? null;
-    // TODO: убрать
     this.email = payload?.email ?? null;
     this.phone = payload?.phone ?? null;
     this.birthdate = payload?.birthdate ?? null;
     this.gender = payload?.gender ?? User.enum.genders.MAN;
     this.role = payload?.role ?? null;
-    this.doctor = payload?.doctor ?? null;
     this.doctor_id = payload?.doctor_id ?? null;
+    this.doctor = payload?.doctor ?? null;
 
     this.childrens_count = payload?.childrens_count ?? 0;
     this.childrens = payload?.childrens ?? [];
