6206a25dd9c96c362f545464dcdacc2ed121f51a
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-05 15:17:53 +0500

    MIS-151_ambulatory-card update VPatientAmbulatoryHeader

---
+++ src/views/patients/VPatient/children/VPatientAmbulatoryCard/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="v-patient-ambulatory">
+    <LayoutContentHeader>
+      <ElPageHeader
+        class="v-patient-ambulatory-header"
+        :title="$t('Base.Back')"
+        @back="goToPatient">
+        <template #content>
+          <div class="v-patient-ambulatory-header-info">
+            <div>{{ $t('Base.AmbulatoryCard') }}</div>
+
+            <ElButton type="primary" text @click="goToPatient"> {{ patient.name }} </ElButton>
+          </div>
+        </template>
+      </ElPageHeader>
+    </LayoutContentHeader>
+  </div>
+</template>
+
+<script>
+import * as icons from '@/enums/icons.enum';
+import { Patient } from '@/models/Patient.model';
+import { PATIENT_ROUTE } from '@/router/patients.routes';
+import LayoutContentHeader from '@/components/layouts/assets/LayoutContentHeader/index.vue';
+
+export default {
+  name: 'VPatientAmbulatoryCard',
+  components: { LayoutContentHeader },
+  emits: ['update:patient'],
+  props: {
+    patient: [Patient, Object],
+    /** @param {Array<Appointment|object>} appointments */
+    appointments: Array,
+    /** @param {{profile: boolean, appointment: boolean}} loading */
+    loading: Object,
+  },
+
+  methods: {
+    goToPatient() {
+      this.$router.push({
+        name: PATIENT_ROUTE.name,
+        params: {
+          id: this.patient.id,
+        },
+      });
+    },
+  },
+
+  setup: () => ({
+    icons,
+  }),
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/base.locales.json" />
+<i18n src="./index.locales.json" />
