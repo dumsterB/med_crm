79bc254924759f79d8bb71b4dbd7bd09ed9e80e1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-07 18:43:45 +0500

    MIS-145_appointment fix DoctorsMenu

--- src/components/doctors/DoctorsMenu/index.vue
+++ src/components/doctors/DoctorsMenu/index.vue
@@ -10,6 +10,7 @@ import {
   DOCTORS_TEMPLATES_ROUTE,
   DOCTORS_SETTINGS_ROUTE,
 } from '@/router/doctors.routes';
+import { PATIENTS_ROUTE } from '@/router/patients.routes';
 import DefaultMenu from '@/components/layouts/assets/DefaultMenu/index.vue';
 
 export default {
@@ -17,11 +18,13 @@ export default {
   components: { DefaultMenu },
   computed: {
     navigation() {
-      return [DASHBOARD_ROUTE, DOCTORS_QUEUE_ROUTE, DOCTORS_TEMPLATES_ROUTE].map((route) => ({
-        path: route.path,
-        title: this.routesOptionsByRouteName[route.name].text,
-        icon: this.routesOptionsByRouteName[route.name].icon,
-      }));
+      return [DASHBOARD_ROUTE, DOCTORS_QUEUE_ROUTE, PATIENTS_ROUTE, DOCTORS_TEMPLATES_ROUTE].map(
+        (route) => ({
+          path: route.path,
+          title: this.routesOptionsByRouteName[route.name].text,
+          icon: this.routesOptionsByRouteName[route.name].icon,
+        })
+      );
     },
 
     routesOptionsByRouteName() {
@@ -33,6 +36,10 @@ export default {
         [DOCTORS_QUEUE_ROUTE.name]: {
           icon: icons.NOTE,
           text: this.$t('Base.Queue'),
+        },
+        [PATIENTS_ROUTE.name]: {
+          icon: icons.USER_TAG,
+          text: this.$t('Base.Patients'),
         },
         [DOCTORS_TEMPLATES_ROUTE.name]: {
           icon: icons.NOTE,
