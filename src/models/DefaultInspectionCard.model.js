aa2597a688c6f4cd052e51ad8233d838bdf81e80
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-07 15:02:25 +0500

    MIS-145_appointment create DefaultInspectionCard model

---
+++ src/models/DefaultInspectionCard.model.js
@@ -0,0 +1,16 @@
+import { InspectionCard } from '@/models/InspectionCard.model';
+
+export class DefaultInspectionCard extends InspectionCard {
+  constructor(payload) {
+    super({ ...payload, type: InspectionCard.enum.types.Default });
+
+    this.complaints = payload?.complaints ?? null;
+    this.anamnesis = payload?.anamnesis ?? null;
+    this.anamnesis_life = payload?.anamnesis_life ?? null;
+    this.operations = payload?.operations ?? null;
+    this.general_state = payload?.general_state ?? null;
+    this.local_status = payload?.local_status ?? null;
+    this.preliminary_diagnosis = payload?.preliminary_diagnosis ?? null;
+    this.survey_plan = payload?.survey_plan ?? null;
+  }
+}
