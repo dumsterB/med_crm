aef509837250a441b42cb3df2aeecc56f5270af0
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-07 20:04:42 +0500

    MIS-145_appointment create DefaultInspectionCardBaseFormItems

---
+++ src/components/appointments/DefaultInspectionCardBaseFormItems/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <ElFormItem :label="field.label" v-for="(field, index) of formFields" :key="index">
+    <component
+      class="create-template-drawer-form__field"
+      :is="field.tag"
+      :placeholder="field.placeholder"
+      :model-value="data[field.name]"
+      autosize
+      :required="field.required"
+      :type="field.type"
+      @update:model-value="updateField(field, $event)">
+      <UiRequiredHiddenInput v-if="field.options?.length" :required="field.required" />
+
+      <ElOption
+        v-if="field.options?.length"
+        v-for="item in field.options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value" />
+    </component>
+  </ElFormItem>
+</template>
+
+<script>
+import { DefaultInspectionCard } from '@/models/DefaultInspectionCard.model';
+
+export default {
+  name: 'DefaultInspectionCardBaseFormItems',
+  props: {
+    data: [DefaultInspectionCard, Object],
+  },
+  computed: {
+    formFields() {
+      return [
+        {
+          label: this.$t('Appointments.InspectionCard.Сomplaints'),
+          type: 'textarea',
+          name: 'complaints',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.Anomnes'),
+          name: 'anamnesis',
+          type: 'textarea',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.Operations'),
+          type: 'textarea',
+          name: 'operations',
+          tag: 'ElInput',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.AnomnesLife'),
+          type: 'textarea',
+          required: true,
+          name: 'anamnesis_life',
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.GeneralState'),
+          type: 'textarea',
+          name: 'general_state',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.LocalStatus'),
+          type: 'textarea',
+          name: 'local_status',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.PreDiagnosis'),
+          type: 'textarea',
+          name: 'preliminary_diagnosis',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.PlanObservation'),
+          type: 'textarea',
+          name: 'survey_plan',
+          required: true,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+        {
+          label: this.$t('Appointments.InspectionCard.Recommendations'),
+          type: 'textarea',
+          name: 'recommendations',
+          required: false,
+          placeholder: this.$t('Base.WriteText'),
+          tag: 'ElInput',
+        },
+      ];
+    },
+  },
+
+  methods: {
+    updateField(field, value) {
+      this.$emit('update:data', { ...this.data, [field.name]: value });
+    },
+  },
+};
+</script>
+
+<i18n src="@/locales/base.locales.json" />
+<i18n src="@/locales/appointments.locales.json" />
+<i18n src="@/locales/templates.locales.json" />
