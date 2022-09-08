6edddbbb233a0ab072f3ee8119cc124b7eac61d4
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-08 20:20:56 +0500

    MIS-145_appointment create APPOINTMENT_ROUTE_TREATMENT_CARD

--- src/router/appointments.routes.js
+++ src/router/appointments.routes.js
@@ -3,7 +3,7 @@ import { onlyManagerMiddleware } from '@/middlewares/onlyManager.middleware';
 import { I18nService } from '@/services/i18n.service';
 
 export const APPOINTMENTS_ROUTE = {
-  name: 'APPOINTMENTS_ROUTE',
+  name: 'APPOINTMENTS',
   path: '/appointments',
   component: 'VAppointments',
   beforeEnter: [onlyLoggedInMiddleware, onlyManagerMiddleware],
@@ -13,7 +13,7 @@ export const APPOINTMENTS_ROUTE = {
 };
 
 export const APPOINTMENT_ROUTE = {
-  name: 'APPOINTMENT_ROUTE',
+  name: 'APPOINTMENT',
   path: '/appointments/:id',
   component: 'VAppointment',
   beforeEnter: [onlyLoggedInMiddleware],
@@ -25,16 +25,22 @@ export const APPOINTMENT_ROUTE = {
 
   childrenMap: {
     APPOINTMENT_ROUTE_DEFAULT_CARD: {
-      name: 'APPOINTMENT_ROUTE_DEFAULT_CARD',
+      name: 'APPOINTMENT_DEFAULT_CARD',
       path: 'default-card',
       _fullPath: '/appointments/:id/default-card',
       component: 'VAppointmentDefaultCard',
     },
     APPOINTMENT_ROUTE_INSPECTION_CARD: {
-      name: 'APPOINTMENT_ROUTE_INSPECTION_CARD',
+      name: 'APPOINTMENT_INSPECTION_CARD',
       path: 'inspection-card',
       _fullPath: '/appointments/:id/inspection-card',
       component: 'VAppointmentDefaultInspectionCard',
+    },
+    APPOINTMENT_ROUTE_TREATMENT_CARD: {
+      name: 'APPOINTMENT_TREATMENT_CARD',
+      path: 'treatment-card',
+      _fullPath: '/appointments/:id/treatment-card',
+      component: 'VAppointmentTreatmentInspectionCard',
     },
   },
   children: [], // утилита сама подставит сюда роуты из childrenMap
