602203477362d95c9316e6c2adec12f742525d50
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-08 19:18:41 +0500

    MIS-145_appointment update Treatment model

--- src/models/Treatment.model.js
+++ src/models/Treatment.model.js
@@ -7,29 +7,27 @@ export class Treatment extends CRUDModel {
 
   /**
    * @param {object} [payload]
-   * @param {number} payload.id
-   * @param {number} payload.parent_id
-   * @param {string} payload.birthdate
-   * @param {string} payload.gender
-   * @param {string} payload.created_at
-   * @param {string} payload.role
-   * @param {number} payload.childrens_count
-   * @param {number} payload.doctor_id
+   * @param {string} payload.title
+   * @param {number} payload.user_id
+   * @param {Patient|User} payload.user
+   * @param {number} payload.price
+   * @param {number} payload.duration
+   * @param {DeleaseCode} payload.delease_code
    * @param {string} payload.status
+   * @param {string|Date} payload.created_at
    */
-
   constructor(payload) {
     super(payload);
 
-    this.id = payload?.id ?? null;
-    this.birthdate = payload?.birthdate ?? null;
-    this.gender = payload?.gender ?? null;
-    this.created_at = payload?.created_at ?? null;
-    this.role = payload?.role ?? null;
-    this.childrens_count = payload?.childrens_count ?? null;
-    this.doctor_id = payload?.doctor_id ?? null;
-    this.parent_id = payload?.parent_id ?? null;
+    this.title = payload?.title ?? null;
+    this.user_id = payload?.user_id ?? null;
+    this.user = payload?.user ?? null;
+    this.price = payload?.price ?? 0;
+    this.duration = payload?.duration ?? 0;
+    this.delease_code = payload?.delease_code ?? null;
     this.status = payload?.status ?? null;
+
+    this.created_at = payload?.created_at ?? null;
   }
 
   static async findByUserId(userId) {
