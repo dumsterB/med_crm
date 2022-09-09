f2a67d8e3f5cc181a36d5dc7658c341438f8694d
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-09 16:08:18 +0500

    MIS-145_appointment update VAppointmentDefaultInspectionCard

--- src/views/appointments/VAppointment/children/VAppointmentDefaultCard/index.js
+++ src/views/appointments/VAppointment/children/VAppointmentDefaultCard/index.js
@@ -6,6 +6,7 @@ import { Appointment } from '@/models/Appointment.model';
 import { PriceService } from '@/services/price.service';
 
 import AppointmentStatusTag from '@/components/appointments/AppointmentStatusTag/index.vue';
+import { APPOINTMENT_ROUTE } from '@/router/appointments.routes';
 
 export default {
   name: 'VAppointmentDefaultCard',
@@ -33,6 +34,13 @@ export default {
         params: { id: this.appointment?.patient_id },
       });
     },
+    appointmentInspectionCardPageLink() {
+      return insertRouteParams({
+        path: APPOINTMENT_ROUTE.childrenMap.APPOINTMENT_ROUTE_INSPECTION_CARD._fullPath,
+        params: { id: this.appointment.id },
+      });
+    },
+
     infoItems() {
       return [
         {
