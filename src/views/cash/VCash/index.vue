7f651e8ddbdd491bbd3d8f80a12ccbbd3d22a07c
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-26 14:53:31 +0500

    MIS-208_cash create empty /cash page

---
+++ src/views/cash/VCash/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <LayoutRegistry content-class="v-cash-content"> Cash </LayoutRegistry>
+</template>
+
+<script>
+import LayoutRegistry from '@/components/layouts/LayoutRegistry/index.vue';
+
+export default {
+  name: 'VCash',
+  components: { LayoutRegistry },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="./index.locales.json" />
