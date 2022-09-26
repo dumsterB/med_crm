3c3262b52a60bae0d5b11ebe8bc4f513f91b9194
Dumster <abduvalievbehr.uz1@gmail.com>
2022-09-26 11:54:50 +0500

    update sidebar and navbar

---
+++ src/components/layouts/assets/layoutScanHeader/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="layout-content-scan">
+    <UiIcon :icon="icons.SCAN" />
+    <span class="layout-content-scan__text">Сканер браслета</span>
+  </div>
+</template>
+
+<script>
+import './index.scss';
+import * as icons from '@/enums/icons.enum.js';
+
+export default {
+  name: 'index',
+  setup: () => ({
+    icons,
+  }),
+};
+</script>
+
+<style scoped></style>
