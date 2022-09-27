2e751c436d29bb7f177b04bf59e5d35291f542c3
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-27 17:02:05 +0500

    MIS-208_cash create CreateOrPayInvoiceModal

---
+++ src/components/invoices/CreateOrPayInvoiceModal/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <ElDialog :model-value="modelValue" @update:model-value="$emit('update:modelValue', $event)">
+    <template #header> Create </template>
+  </ElDialog>
+</template>
+
+<script>
+import { Invoice } from '@/models/Invoice.model';
+
+export default {
+  name: 'CreateOrPayInvoiceModal',
+  props: {
+    modelValue: Boolean,
+    data: [Invoice, Object],
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/invoices.locales.json" />
+<i18n src="./index.locales.json" />
