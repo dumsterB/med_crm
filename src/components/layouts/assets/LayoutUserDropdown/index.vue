b50145450bb92c18f9f2d6ea5426005c3837f3ef
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-28 17:32:06 +0500

    MIS-209_redesign-auth fix LayoutUserDropdown

--- src/components/layouts/assets/LayoutUserDropdown/index.vue
+++ src/components/layouts/assets/LayoutUserDropdown/index.vue
@@ -1,20 +1,20 @@
 <template>
-  <ElPopover class="layout-user-dropdown" trigger="click" placement="bottom" width="300px">
+  <UiUserAvatarInfo :user="user" secondary-field-key="email" />
+  <!--  <ElPopover class="layout-user-dropdown" trigger="click" placement="bottom" width="300px">
     <template #reference>
       <div class="layout-user-dropdown__reference layout-user-dropdown-reference">
-        <UiUserAvatarInfo :user="user" secondary-field-key="email" />
-<!--        <UiIcon class="layout-user-dropdown-reference__icon" :icon="icons.CHEVRON_DOWN" />-->
+        <UiIcon class="layout-user-dropdown-reference__icon" :icon="icons.CHEVRON_DOWN" />
       </div>
     </template>
 
-<!--    <div class="layout-user-dropdown__content layout-user-dropdown-content">-->
-<!--      <div class="layout-user-dropdown-part">-->
-<!--        <div class="layout-user-dropdown-part__title">{{ $t('Base.ChangeLang') }}</div>-->
+    <div class="layout-user-dropdown__content layout-user-dropdown-content">
+      <div class="layout-user-dropdown-part">
+        <div class="layout-user-dropdown-part__title">{{ $t('Base.ChangeLang') }}</div>
 
-<!--        <LangSelect class="layout-user-dropdown-part__value" />-->
-<!--      </div>-->
-<!--    </div>-->
-  </ElPopover>
+        <LangSelect class="layout-user-dropdown-part__value" />
+      </div>
+    </div>
+  </ElPopover>-->
 </template>
 
 <script>
