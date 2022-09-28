b4441430845c1ae9ba48afa0ce5c4b72630ce1b0
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-28 12:41:17 +0500

    MIS-208_cash create PatientsSearchSelect

---
+++ src/components/patients/PatientsSearchSelect/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <UiModelsAutocompleteSearch
+    :model-value="modelValue"
+    :model-for-use="Patient"
+    v-bind="$props"
+    @update:model-value="$emit('update:modelValue', $event)"
+    @select="$emit('select', $event)" />
+</template>
+
+<script>
+import { Patient } from '@/models/Patient.model';
+import UiModelsAutocompleteSearch from '@/components/ui/UiModelsAutocompleteSearch/index.vue';
+
+export default {
+  name: 'PatientsSearchSelect',
+  components: { UiModelsAutocompleteSearch },
+  emits: ['update:modelValue', 'select'],
+  props: {
+    modelValue: Number,
+    withScanner: Boolean,
+    // + пропсы для UiModelsAutocompleteSearch
+  },
+
+  setup: () => ({
+    Patient: Patient,
+  }),
+};
+</script>
