39295904ea479ff0fd0b55c11d519cc77513b5d1
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-09-28 13:16:17 +0500

    MIS-208_cash add 'count' field on InvoicePaymentSubject

--- src/models/InvoicePaymentSubject.model.js
+++ src/models/InvoicePaymentSubject.model.js
@@ -5,12 +5,14 @@ export class InvoicePaymentSubject {
   /**
    * @param {object} [payload]
    * @param {string} payload.description
+   * @param {number} payload.count
    * @param {number} payload.discount
    * @param {string} payload.type
    * @param {Service, ServiceGroup} payload.subject
    */
   constructor(payload) {
     this.description = payload?.description ?? null;
+    this.count = payload?.count ?? 1;
     this.discount = payload?.discount ?? null;
     this.type = payload?.type ?? null;
     this.subject = payload?.subject ?? null;
