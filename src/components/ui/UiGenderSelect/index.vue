085cb5727281db92bfd5c62d90c5c37aa6562f14
Dumster <abduvalievbehr.uz1@gmail.com>
2022-10-01 10:14:05 +0500

    MIS-212 - Update genders

--- src/components/ui/UiGenderSelect/index.vue
+++ src/components/ui/UiGenderSelect/index.vue
@@ -1,18 +1,29 @@
 <template>
-  <ElSelect
-    class="ui-gender-select"
-    :model-value="modelValue"
-    v-bind="$attrs"
-    @update:model-value="$emit('update:modelValue', $event)">
-    <ElOption
-      v-for="gender in genders"
-      :key="gender.value"
-      :label="gender.label"
-      :value="gender.value" />
-  </ElSelect>
+  <!--  <ElSelect-->
+  <!--    class="ui-gender-select"-->
+  <!--    :model-value="modelValue"-->
+  <!--    v-bind="$attrs"-->
+  <!--    @update:model-value="$emit('update:modelValue', $event)">-->
+  <!--    <ElOption-->
+  <!--      v-for="gender in genders"-->
+  <!--      :key="gender.value"-->
+  <!--      :label="gender.label"-->
+  <!--      :value="gender.value" />-->
+  <!--  </ElSelect>-->
+  <div class="ui-gender-select">
+    <div
+      v-for="gender of genders"
+      :key="gender"
+      :class="[gender.active === true ? 'ui-gender-select-active' : 'ui-gender-select-item']"
+      @click="genderHandler(gender)">
+      <UiIcon :icon="gender.value == 'man' ? icons.MALE : icons.FEMALE" />
+      {{ gender.label }}
+    </div>
+  </div>
 </template>
 
 <script>
+import * as icons from '@/enums/icons.enum.js';
 import { User } from '@/models/User.model.js';
 
 export default {
@@ -26,10 +37,21 @@ export default {
       return Object.keys(User.enum.genders).map((key) => ({
         label: this.$t(`User.Genders.${User.enum.genders[key]}`),
         value: User.enum.genders[key],
+        active: false,
       }));
     },
   },
+  methods: {
+    genderHandler(gender) {
+      gender.active = !gender.active;
+      console.log(gender);
+    },
+  },
+  setup: () => ({
+    icons,
+  }),
 };
 </script>
 
 <style lang="scss" src="./index.scss" />
+<i18n src="@/locales/user.locales.json" />
