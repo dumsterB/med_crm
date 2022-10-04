4612538b412184cdd75724b9dd0957b704877b15
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-04 12:52:16 +0500

    MIS-215_templates delete trash code from DefaultInspectionCard

--- src/models/DefaultInspectionCard.model.js
+++ src/models/DefaultInspectionCard.model.js
@@ -20,15 +20,5 @@ export class DefaultInspectionCard extends InspectionCard {
   /** @param {DefaultInspectionCardConstructorPayload} [payload] */
   constructor(payload) {
     super({ ...payload, type: InspectionCard.enum.types.Default });
-
-    this.complaints = payload?.complaints ?? null;
-    this.anamnesis = payload?.anamnesis ?? null;
-    this.anamnesis_life = payload?.anamnesis_life ?? null;
-    this.operations = payload?.operations ?? null;
-    this.general_state = payload?.general_state ?? null;
-    this.local_status = payload?.local_status ?? null;
-    this.preliminary_diagnosis = payload?.preliminary_diagnosis ?? null;
-    this.survey_plan = payload?.survey_plan ?? null;
-    this.recommendations = payload?.recommendations ?? null;
   }
 }
