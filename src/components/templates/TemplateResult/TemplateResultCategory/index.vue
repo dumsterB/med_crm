0f615cfd8ac50a60cb66799fa9c967aa00c06999
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-05 17:48:47 +0500

    MIS-215_templates create TemplatesResultCategory

---
+++ src/components/templates/TemplateResult/TemplateResultCategory/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <ElCard class="template-result-category" shadow="never">
+    <div class="template-result-category__title">{{ category.label }}</div>
+    <div class="template-result-category__blocks">
+      <TemplateResultBlock
+        v-for="block in blocks"
+        :key="block.id"
+        :block="block"
+        @update:block="$emit('update:block', $event)"
+        @change="$emit('change')" />
+    </div>
+  </ElCard>
+</template>
+
+<script>
+import { InspectionCardCategory } from '@/models/InspectionCardCategory.model';
+import TemplateResultBlock from '@/components/templates/TemplateResult/TemplateResultBlock/index.vue';
+
+export default {
+  name: 'TemplateResultCategory',
+  components: { TemplateResultBlock },
+  emits: ['update:block', 'change'],
+  props: {
+    category: [InspectionCardCategory, Object],
+    /** @type {Array<InspectionCardBlock|object>} blocks */
+    blocks: Array,
+    editable: Boolean,
+  },
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
