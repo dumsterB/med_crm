aabd498eb7d2635dccb4b7de32903e27eb881088
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-06 19:55:54 +0500

    MIS-215_templates delete trash code

--- src/views/appointments/VAppointment/children/VAppointmentTreatmentInspectionCard/index.vue
+++ src/views/appointments/VAppointment/children/VAppointmentTreatmentInspectionCard/index.vue
@@ -141,20 +141,7 @@ export default {
 
   methods: {
     callPrint() {
-      const modal = document.getElementById('treatments');
-      const cloned = modal.cloneNode(true);
-      let section = document.getElementById('print');
-
-      if (!section) {
-        section = document.createElement('div');
-        section.id = 'print';
-        document.body.appendChild(section);
-      }
-
-      section.innerHTML = '';
-      section.appendChild(cloned);
       window.print();
-      document.getElementById('print').remove();
     },
     goToAppointment() {
       this.$router.push({
