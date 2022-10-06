9853b20a453b09307c20cf622cdaee6bcaa02c5b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-06 15:28:46 +0500

    MIS-215_templates add DOWNLOAD icon

--- src/components/SVG.vue
+++ src/components/SVG.vue
@@ -613,6 +613,10 @@
         stroke="currentColor"
         stroke-width="0.2" />
     </symbol>
+
+    <symbol :id="icons.DOWNLOAD" viewBox="0 0 24 24">
+      <path fill="currentColor" d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" />
+    </symbol>
   </svg>
 </template>
 
