b7e7c62bb443399578e360a781a2a118183fd42b
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-07 17:25:27 +0500

    MIS-225_ambulatory-card update VAppointmentAmbulatoryCard

--- src/views/patients/VPatient/children/VPatientAmbulatoryCard/index.vue
+++ src/views/patients/VPatient/children/VPatientAmbulatoryCard/index.vue
@@ -15,11 +15,20 @@
       </ElPageHeader>
     </LayoutContentHeader>
 
+    <PatientCardRow :items="infoItems" />
+
     <AmbulatoryCard
       class="v-patient-ambulatory__card"
       :patient="patient"
       :treatments="treatments"
       @update:patient="updatePatientFromAmbulatoryCard" />
+
+    <TreatmentsTable
+      :items="treatments"
+      :total="treatments?.length"
+      :perPage="treatments?.length"
+      :page="1"
+      :actionShow="true" />
   </div>
 </template>
 
@@ -29,19 +38,44 @@ import { Patient } from '@/models/Patient.model';
 import { PATIENT_ROUTE } from '@/router/patients.routes';
 import LayoutContentHeader from '@/components/layouts/assets/LayoutContentHeader/index.vue';
 import AmbulatoryCard from '@/components/patients/AmbulatoryCard/index.vue';
+import TreatmentsTable from '@/components/treatments/TreatmentsTable/index.vue';
+import PatientCardRow from '@/components/patients/PatientCardRow/index.vue';
 
 export default {
   name: 'VPatientAmbulatoryCard',
-  components: { AmbulatoryCard, LayoutContentHeader },
+  components: { PatientCardRow, AmbulatoryCard, LayoutContentHeader, TreatmentsTable },
   emits: ['update:patient'],
   props: {
     patient: [Patient, Object],
     /** @param {Array<Appointment|object>} appointments */
     appointments: Array,
     /** @param {Array<Treatments|object>} treatments */
-    treatments:Array,
+    treatments: Array,
     /** @param {{profile: boolean, appointment: boolean}} loading */
     loading: Object,
+  },
+  computed: {
+    infoItems() {
+      return [
+        {
+          gender: this.patient.gender,
+          label: this.$t('Patients.NamePatient'),
+          value: this.patient.name || '',
+        },
+        {
+          label: this.$t('User.Phone'),
+          value: this.patient.phone || '',
+        },
+        {
+          label: this.$t('User.Birthdate'),
+          value: this.patient.birthdate || '',
+        },
+        {
+          label: this.$t('User.Gender'),
+          value: this.$t('User.Genders.' + this.patient.gender) || '',
+        },
+      ];
+    },
   },
 
   methods: {
@@ -68,4 +102,6 @@ export default {
 
 <style lang="scss" src="./index.scss" />
 <i18n src="@/locales/base.locales.json" />
+<i18n src="@/locales/user.locales.json" />
+<i18n src="@/locales/patients.locales.json" />
 <i18n src="./index.locales.json" />
