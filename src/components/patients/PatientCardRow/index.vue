cedc0f0c0fa4a04b771e43d041e6d152c0cc0197
Dumster <abduvalievbehr.uz1@gmail.com>
2022-10-07 11:37:03 +0500

    MIS-225 - Update and fix comments

---
+++ src/components/patients/PatientCardRow/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="patient-card">
+    <ElCard class="patient-card">
+      <div class="patient-card-info_avatar">
+        <div>
+          <UiAvatar size="large" :gender="patient.gender"></UiAvatar>
+        </div>
+        <div>
+          <p class="patient-card-info__title">
+            {{ $t('Patients.NamePatient') }}
+          </p>
+          <p class="patient-card-info__description">
+            {{ patient.name }}
+          </p>
+        </div>
+      </div>
+      <div class="patient-card-info">
+        <p class="patient-card-info__title">
+          {{ $t('User.Phone') }}
+        </p>
+        <p class="patient-card-info__description">
+          {{ patient.phone }}
+        </p>
+      </div>
+      <div class="patient-card-info">
+        <p class="patient-card-info__title">
+          {{ $t('User.Birthdate') }}
+        </p>
+        <p class="patient-card-info__description">
+          {{ patient.birthdate }}
+        </p>
+      </div>
+      <div class="patient-card-info">
+        <p class="patient-card-info__title">
+          {{ $t('User.Gender') }}
+        </p>
+        <p class="patient-card-info__description">
+          {{ patient.gender }}
+        </p>
+      </div>
+    </ElCard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  props:{
+    patient: {},
+  }
+};
+</script>
+
+<style lang="scss" src="./index.scss" />
+<i18n src="@/locales/patients.locales.json" />
+<i18n src="@/locales/user.locales.json" />
