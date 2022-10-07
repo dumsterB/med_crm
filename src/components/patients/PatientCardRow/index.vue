11819acb10b3c27e3158a4432670dcce7a49823f
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-07 17:39:05 +0500

    MIS-225_ambulatory-card refactored PatientCardRow

--- src/components/patients/PatientCardRow/index.vue
+++ src/components/patients/PatientCardRow/index.vue
@@ -1,27 +1,30 @@
 <template>
-  <div class="patient-card">
-    <ElCard :shadow="shadow" class="patient-card">
-      <div class="patient-card-info" v-for="patient of items">
-        <div class="patient-card-info_avatar" v-if="patient.gender">
-          <UiAvatar size="large" :gender="patient.gender"></UiAvatar>
-        </div>
-        <div>
-          <p class="patient-card-info__title">
-            {{ patient.label }}
-          </p>
-          <p class="patient-card-info__description">
-            {{ patient.value }}
-          </p>
-        </div>
+  <ElCard class="patient-card" :shadow="shadow">
+    <UiUserAvatarInfo class="patient-card__user" :user="patient" />
+
+    <div class="patient-card__info patient-card-info" v-for="item in items" :key="item.value">
+      <div class="patient-card-info__label">
+        <slot name="label" :item="item">{{ item.label }}</slot>
       </div>
-    </ElCard>
-  </div>
+      <div class="patient-card-info__value">
+        <slot name="value" :item="item">{{ item.value }}</slot>
+      </div>
+    </div>
+
+    <div class="patient-card__actions patient-card-actions">
+      <slot name="actions"></slot>
+    </div>
+  </ElCard>
 </template>
 
 <script>
+import { Patient } from '@/models/Patient.model';
+
 export default {
   name: 'PatientCardRow',
   props: {
+    patient: [Patient, Object],
+    /** @type {Array<{label: string, value: string|number}>} items */
     items: Array,
     shadow: {
       default: 'never',
