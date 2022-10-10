342e9298628061cdbe9b1cdad738558f19a20e18
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-10 20:32:44 +0500

    dev add NOT_REDIRECT const in query.enum.js

--- src/enums/query.enum.js
+++ src/enums/query.enum.js
@@ -3,3 +3,4 @@ export const PAGE = 'page';
 export const PER_PAGE = 'per_page';
 export const QUERY_FIELD = 'query_field';
 export const QUERY_TYPE = 'query_type';
+export const NOT_REDIRECT = 'not_redirect';
