6cdd96bc36b446ec706bfc9481ef69ae3b4e69d7
Ravshan Selimov <51333533+ravshan01@users.noreply.github.com>
2022-10-10 19:31:06 +0500

    dev TemplateResultBlock supported default slot

--- src/components/templates/TemplateResult/TemplateResultBlock/index.vue
+++ src/components/templates/TemplateResult/TemplateResultBlock/index.vue
@@ -3,83 +3,85 @@
     <div class="template-result-block__title">{{ block.label }}</div>
 
     <div class="template-result-block__content">
-      <!--  Input  -->
-      <ElInput
-        v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Input"
-        :model-value="block.answer?.value"
-        :placeholder="$t('Base.PleaseInput')"
-        v-bind="meta"
-        @update:model-value="updateValue"
-        @change="$emit('change')" />
+      <slot name="default" :block="block">
+        <!--  Input  -->
+        <ElInput
+          v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Input"
+          :model-value="block.answer?.value"
+          :placeholder="$t('Base.PleaseInput')"
+          v-bind="meta"
+          @update:model-value="updateValue"
+          @change="$emit('change')" />
 
-      <!--  Textarea  -->
-      <ElInput
-        v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Textarea"
-        :model-value="block.answer?.value"
-        type="textarea"
-        :placeholder="$t('Base.PleaseInput')"
-        v-bind="meta"
-        @update:model-value="updateValue"
-        @change="$emit('change')" />
+        <!--  Textarea  -->
+        <ElInput
+          v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Textarea"
+          :model-value="block.answer?.value"
+          type="textarea"
+          :placeholder="$t('Base.PleaseInput')"
+          v-bind="meta"
+          @update:model-value="updateValue"
+          @change="$emit('change')" />
 
-      <!--  Select  -->
-      <ElSelect
-        v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Select"
-        :model-value="value"
-        v-bind="meta"
-        @update:modelValue="updateValue"
-        @select="$emit('change')">
-        <ElOption
-          v-for="option in block.options"
-          :key="option.value"
-          :label="option.label"
-          :value="option.value" />
-      </ElSelect>
+        <!--  Select  -->
+        <ElSelect
+          v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Select"
+          :model-value="value"
+          v-bind="meta"
+          @update:modelValue="updateValue"
+          @select="$emit('change')">
+          <ElOption
+            v-for="option in block.options"
+            :key="option.value"
+            :label="option.label"
+            :value="option.value" />
+        </ElSelect>
 
-      <!--  Checkbox  -->
-      <ElCheckboxGroup
-        v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Checkbox"
-        :model-value="value || []"
-        @update:model-value="updateValue"
-        @change="$emit('change')">
-        <ElCheckbox
-          v-for="option in block.options"
-          :key="option.label"
-          :label="option.value"
-          :disabled="meta.disabled">
-          {{ option.label }}
-        </ElCheckbox>
-      </ElCheckboxGroup>
+        <!--  Checkbox  -->
+        <ElCheckboxGroup
+          v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Checkbox"
+          :model-value="value || []"
+          @update:model-value="updateValue"
+          @change="$emit('change')">
+          <ElCheckbox
+            v-for="option in block.options"
+            :key="option.label"
+            :label="option.value"
+            :disabled="meta.disabled">
+            {{ option.label }}
+          </ElCheckbox>
+        </ElCheckboxGroup>
 
-      <!--  Date  -->
-      <ElDatePicker
-        v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Date"
-        :model-value="block.answer?.value"
-        :disabled="block.meta.disabled"
-        value-format="DD.MM.YYYY"
-        :placeholder="$t('Base.PleaseSelect')"
-        @update:model-value="updateValue"
-        @change="$emit('change')" />
+        <!--  Date  -->
+        <ElDatePicker
+          v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.Date"
+          :model-value="block.answer?.value"
+          :disabled="block.meta.disabled"
+          value-format="DD.MM.YYYY"
+          :placeholder="$t('Base.PleaseSelect')"
+          @update:model-value="updateValue"
+          @change="$emit('change')" />
 
-      <!--  File  -->
-      <UiUpload
-        v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.File"
-        class="template-result-block__file-upload"
-        :files="files"
-        :disabled="meta.disabled"
-        :multiple="meta.multiple"
-        @file:add="addFileHandler"
-        @file:delete="deleteFileHandler" />
+        <!--  File  -->
+        <UiUpload
+          v-if="block.answer_type === InspectionCardBlock.enum.answerTypes.File"
+          class="template-result-block__file-upload"
+          :files="files"
+          :disabled="meta.disabled"
+          :multiple="meta.multiple"
+          @file:add="addFileHandler"
+          @file:delete="deleteFileHandler" />
 
-      <!--  Custom Answer  -->
-      <ElInput
-        v-if="block.meta.enable_custom_answer"
-        :model-value="customValue"
-        type="textarea"
-        :placeholder="$t('Templates.OtherAnswer')"
-        :rows="4"
-        @update:model-value="updateCustomValue"
-        @change="$emit('change')" />
+        <!--  Custom Answer  -->
+        <ElInput
+          v-if="block.meta.enable_custom_answer"
+          :model-value="customValue"
+          type="textarea"
+          :placeholder="$t('Templates.OtherAnswer')"
+          :rows="4"
+          @update:model-value="updateCustomValue"
+          @change="$emit('change')" />
+      </slot>
     </div>
   </ElCard>
 </template>
