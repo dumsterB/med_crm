7dd58a23a4678206b591529df1eff0b5e18cd42b
Dumster <abduvalievbehr.uz1@gmail.com>
2022-10-10 16:59:27 +0500

    MIS-fix - Update patient card row

--- src/components/patients/PatientCardRow/index.vue
+++ src/components/patients/PatientCardRow/index.vue
@@ -1,44 +1,46 @@
 <template>
-  <div class="patient-card">
-    <ElCard :shadow="shadow" class="patient-card">
-      <div class="patient-card-info" v-for="patient of items">
-        <div class="patient-card-info_avatar" v-if="patient.gender">
-          <UiAvatar size="large" :gender="patient.gender"></UiAvatar>
-        </div>
-        <div>
-          <p class="patient-card-info__title">
-            {{ patient.label }}
-          </p>
-          <p class="patient-card-info__description">
-            {{ patient.value }}
-          </p>
-          <div class="patient-card-info-actions">
-            <ElButton type="primary" v-if="patient.action">{{ patient.text }}</ElButton>
-          </div>
-        </div>
+  <ElCard class="patient-card" :shadow="shadow">
+    <UiUserAvatarInfo class="patient-card__user" :user="patient" :link="patientPageLink" />
+
+    <div class="patient-card__info patient-card-info" v-for="item in items" :key="item.value">
+      <div class="patient-card-info__label">
+        <slot name="label" :item="item">{{ item.label }}</slot>
       </div>
-    </ElCard>
-  </div>
+      <div class="patient-card-info__value">
+        <slot name="value" :item="item">{{ item.value }}</slot>
+      </div>
+    </div>
+
+    <div class="patient-card__actions patient-card-actions">
+      <slot name="actions"></slot>
+    </div>
+  </ElCard>
 </template>
 
 <script>
-import { insertRouteParams } from '@/utils/router.utils';
+import { Patient } from '@/models/Patient.model';
 import { PATIENT_ROUTE } from '@/router/patients.routes';
+import { insertRouteParams } from '@/utils/router.utils';
 
 export default {
   name: 'PatientCardRow',
   props: {
+    patient: [Patient, Object],
+    /** @type {Array<{label: string, value: string|number}>} items */
     items: Array,
     shadow: {
       default: 'never',
       type: String,
     },
   },
+
   computed: {
-    ambulatoryCardPageLink() {
+    patientPageLink() {
       return insertRouteParams({
-        path: PATIENT_ROUTE.childrenMap.PATIENT_ROUTE_AMBULATORY_CARD._fullPath,
-        params: { id: this.data.id },
+        path: PATIENT_ROUTE.path,
+        params: {
+          id: this.patient.id,
+        },
       });
     },
   },
